bd7b357b40165074b3e314ae66c439e1407133ab
Hao Sun <PSDhao@users.noreply.github.com>
2018-02-09 13:42:30 -0500

    Add project property plugin and jar path model

---
+++ src/main/java/beam/providerBuilder/builderUtil/gradle/DefaultJarPathModel.java
@@ -0,0 +1,18 @@
+package beam.providerBuilder.builderUtil.gradle;
+
+import java.io.Serializable;
+
+public class DefaultJarPathModel implements Serializable, JarPathModel {
+    private final String name;
+    private final String version;
+
+    public DefaultJarPathModel(String name, String version) {
+        this.name = name;
+        this.version = version;
+    }
+
+    @Override
+    public String getJarPath() {
+        return String.format("%s-%s.jar", name, version);
+    }
+}
---
+++ src/main/java/beam/providerBuilder/builderUtil/gradle/JarPathModel.java
@@ -0,0 +1,5 @@
+package beam.providerBuilder.builderUtil.gradle;
+
+public interface JarPathModel {
+    String getJarPath();
+}
