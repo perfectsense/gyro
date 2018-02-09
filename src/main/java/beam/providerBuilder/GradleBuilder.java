840924fc89a1360a747d7734c51c178d32705c6d
Hao Sun <PSDhao@users.noreply.github.com>
2018-02-09 13:45:39 -0500

    Generate gradle builds name

--- src/main/java/beam/providerBuilder/GradleBuilder.java
+++ src/main/java/beam/providerBuilder/GradleBuilder.java
@@ -1,10 +1,15 @@
 package beam.providerBuilder;
 
+import beam.providerBuilder.builderUtil.gradle.JarPathModel;
+import com.google.common.base.Charsets;
+import com.psddev.dari.util.IoUtils;
 import org.gradle.tooling.BuildLauncher;
 import org.gradle.tooling.GradleConnector;
+import org.gradle.tooling.ModelBuilder;
 import org.gradle.tooling.ProjectConnection;
 
 import java.io.File;
+import java.io.FileWriter;
 
 public class GradleBuilder extends ProviderBuilder {
 
@@ -12,7 +17,7 @@ public class GradleBuilder extends ProviderBuilder {
     public boolean validate(String path) {
         path = path + File.separator + "build.gradle";
         File buildFile = new File(path);
-        if(buildFile.exists() && !buildFile.isDirectory()) {
+        if (buildFile.exists() && !buildFile.isDirectory()) {
             return true;
         } else {
             return false;
@@ -24,16 +29,32 @@ public class GradleBuilder extends ProviderBuilder {
         ProjectConnection connection = GradleConnector.newConnector()
                 .forProjectDirectory(new File(path))
                 .connect();
+
+        String jarName = null;
         try {
+            String scriptContent = IoUtils.toString(getClass().getResourceAsStream("/init.gradle"), Charsets.UTF_8);
+            File initScript = File.createTempFile("init", ".gradle");
+            initScript.deleteOnExit();
+            String initSrciptPath = initScript.getCanonicalPath();
+            FileWriter fileWriter = new FileWriter(initScript);
+            fileWriter.write(scriptContent);
+            fileWriter.flush();
+            fileWriter.close();
+
+            ModelBuilder<JarPathModel> jarPathModelBuilder = connection.model(JarPathModel.class);
+            jarPathModelBuilder.withArguments("--init-script", initSrciptPath);
+            JarPathModel model = jarPathModelBuilder.get();
+            jarName = model.getJarPath();
+
             BuildLauncher build = connection.newBuild();
-            build.forTasks("dist");
             build.run();
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             connection.close();
         }
 
-        return path + "/build/libs/beam.git-1.0.jar";
+        return String.format("%s/build/libs/%s", path, jarName);
     }
 }
