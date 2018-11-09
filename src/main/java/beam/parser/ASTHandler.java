de5e117418e1db02f860b7286ae1994d2c6aaa74
Hao Sun <PSDhao@users.noreply.github.com>
2018-11-09 15:59:02 -0500

    Keep plugin loading stuff which maybe useful in the future

--- src/main/java/beam/parser/ASTHandler.java
+++ src/main/java/beam/parser/ASTHandler.java
@@ -1,19 +1,15 @@
 package beam.parser;
 
 import beam.core.*;
-import beam.core.diff.*;
 import beam.fetcher.PluginFetcher;
+import beam.lang.BeamConfig;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Throwables;
 import com.psddev.dari.util.ObjectUtils;
-import com.psddev.dari.util.StringUtils;
 import org.reflections.Reflections;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
 
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.*;
 import java.util.*;
@@ -54,7 +50,7 @@ public class ASTHandler {
                 .setUrls(ClasspathHelper.forPackage(packageName)), ClasspathHelper.forPackage("beam.core"));
 
         String className = null;
-        for (Class<? extends BeamObject> resource : reflections.getSubTypesOf(BeamObject.class)) {
+        for (Class<? extends BeamConfig> resource : reflections.getSubTypesOf(BeamConfig.class)) {
             ConfigKey configKey = resource.getAnnotation(ConfigKey.class);
             if (configKey != null && resourceKey.equals(configKey.value())) {
                 className = resource.getName();
@@ -104,205 +100,6 @@ public class ASTHandler {
 
         } catch (Exception e) {
             throw new BeamException(String.format("Unable to populate %s with %s in %s!", key, value, object), e);
-        }
-    }
-
-    public static BeamObject loadBeamObject(Stack<BeamObject> objectStack, Set<BeamResource> pending) {
-        BeamObject object = objectStack.pop();
-        if (object instanceof BeamResource) {
-            BeamResource resource = (BeamResource) object;
-            if (!objectStack.isEmpty() && objectStack.peek() instanceof BeamResource) {
-                BeamResource parent = (BeamResource) objectStack.peek();
-                parent.getChildren().add(resource);
-                resource.setParent(parent);
-
-                parent.dependents().add(resource);
-                resource.dependencies().add(parent);
-
-            } else if (objectStack.isEmpty()) {
-                pending.add(resource);
-            }
-        }
-
-        return object;
-    }
-
-    public static BeamObject createBeamObject(String pluginPackage, String pluginClass) {
-        String className;
-        try {
-            className = getClassName(pluginPackage, pluginClass);
-
-            java.net.URLClassLoader loader = (java.net.URLClassLoader) ClassLoader.getSystemClassLoader();
-            Class<?> clazz = loader.loadClass(className);
-            return (BeamObject) clazz.newInstance();
-
-        } catch (Exception e) {
-           throw new BeamException(String.format("Unable to create resource %s::%s", pluginPackage, pluginClass), e);
-        }
-    }
-
-    private static void adjustCurrentResource(BeamResource currentResource, BeamResource pendingResource, Set<BeamResource> current, Set<BeamResource> adjustCurrent) {
-        Iterator<BeamResource> iter = current.iterator();
-        BeamResource candidate;
-        BeamResource targetResource = null;
-        // change to a map will be much faster
-        while (iter.hasNext()) {
-            candidate = iter.next();
-            ResourceDiffKey pendingKey = new ResourceDiffKey(pendingResource.diffIds());
-            ResourceDiffKey currentKey = new ResourceDiffKey(candidate.diffIds());
-            if (pendingKey.equals(currentKey)) {
-                targetResource = candidate;
-                iter.remove();
-                if (currentResource == null) {
-                    adjustCurrent.add(candidate);
-                } else {
-                    currentResource.getChildren().add(candidate);
-                }
-
-                break;
-            }
-        }
-
-        if (targetResource != null) {
-            for (Object child : pendingResource.getChildren()) {
-                if (child instanceof BeamResource) {
-                    BeamResource childResource = (BeamResource) child;
-                    adjustCurrentResource(targetResource, childResource, current, adjustCurrent);
-                }
-            }
-        }
-    }
-
-    private static void adjustCurrentConfigs(Set<BeamResource> current, Set<BeamResource> adjustCurrent, Set<BeamResource> pending) {
-        for (BeamResource pendingResource : pending) {
-            adjustCurrentResource(null, pendingResource, current, adjustCurrent);
-        }
-
-        adjustCurrent.addAll(current);
-    }
-
-    private static void adjust(BeamResource currentResource, Object currentValue, Map<String, Set<BeamResource>> currentByResourceType, String resourceClass, String resourceProperty) throws IntrospectionException, IllegalAccessException, InvocationTargetException {
-        if (!ObjectUtils.isBlank(currentValue)) {
-            for (String className : currentByResourceType.keySet()) {
-                if (className.endsWith(resourceClass)) {
-                    Set<BeamResource> resourceSet = currentByResourceType.get(className);
-                    for (BeamResource reference : resourceSet) {
-                        if (currentValue.equals(DiffUtil.getPropertyValue(reference, resourceClass, resourceProperty))) {
-                            currentResource.dependencies().add(reference);
-                            reference.dependents().add(currentResource);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private static void adjustCurrentDependencies(Set<BeamResource> current, Map<String, Set<BeamResource>> currentByResourceType) {
-        for (BeamResource currentResource : current) {
-            try {
-                for (PropertyDescriptor p : Introspector.getBeanInfo(currentResource.getClass()).getPropertyDescriptors()) {
-                    Method reader = p.getReadMethod();
-                    if (reader == null) {
-                        continue;
-                    }
-
-                    Class<?> returnType = reader.getReturnType();
-                    Object currentValue = reader.invoke(currentResource);
-                    ResourceReferenceProperty propertyAnnotation = reader.getAnnotation(ResourceReferenceProperty.class);
-
-                    if (propertyAnnotation != null) {
-                        String annotation = propertyAnnotation.value();
-                        if (StringUtils.isBlank(annotation)) {
-                            // throw new BeamException("empty annotation");
-                        }
-
-                        String resourceClass = annotation.split("\\.")[0];
-                        String resourceProperty = annotation.split("\\.")[1];
-
-                        if (Collection.class.isAssignableFrom(returnType)) {
-                            if (currentValue != null) {
-                                Collection collection = (Collection) currentValue;
-                                Iterator iterator = collection.iterator();
-                                while (iterator.hasNext()) {
-                                    Object value = iterator.next();
-                                    adjust(currentResource, value, currentByResourceType, resourceClass, resourceProperty);
-                                }
-                            }
-                        } else {
-                            adjust(currentResource, currentValue, currentByResourceType, resourceClass, resourceProperty);
-                        }
-                    }
-                }
-            } catch (IllegalAccessException |
-                    IntrospectionException error) {
-                throw new IllegalStateException(error);
-
-            } catch (InvocationTargetException error) {
-                throw Throwables.propagate(error);
-            }
-        }
-    }
-
-    private void tryToKeep(List<Diff<?, ?, ?>> diffs) {
-        for (Diff<?, ?, ?> diff : diffs) {
-            for (Change<?> change : diff.getChanges()) {
-                if (change instanceof ResourceUpdate) {
-                    ((ResourceUpdate) change).tryToKeep();
-                }
-
-                tryToKeep(change.getDiffs());
-            }
-        }
-    }
-
-    public static ResourceDiff exitBeamRoot(Set<BeamResource> pending, Set<ChangeType> changeTypes, Map<String, BeamProvider> providerTable) {
-        BeamProvider beamProvider = null;
-        for (BeamProvider oneProvider : providerTable.values()) {
-            beamProvider = oneProvider;
-        }
-
-        Reflections reflections = new Reflections("beam");
-        Set<BeamResource> current = new HashSet<>();
-
-        Map<String, Set<BeamResource>> currentByResourceType = new HashMap<>();
-        Set<BeamResource> adjustCurrent = new HashSet<>();
-
-        long startTime = System.currentTimeMillis();
-        for (Class<? extends BeamResource> resourceClass : reflections.getSubTypesOf(BeamResource.class)) {
-            try {
-                ConfigKey configKey = resourceClass.getAnnotation(ConfigKey.class);
-                if (configKey == null) {
-                    continue;
-                }
-
-                BeamResource resource = resourceClass.newInstance();
-                Set<BeamResource> resourceSet = new HashSet<>();
-                resource.init(beamProvider, null, resourceSet);
-                current.addAll(resourceSet);
-                currentByResourceType.put(resourceClass.getName(), resourceSet);
-            } catch (IllegalAccessException | InstantiationException error) {
-                throw new BeamException(String.format("Unable to load resource from %s", resourceClass), error);
-            }
-        }
-
-        long endTime = System.currentTimeMillis();
-
-        System.out.println("load current took " + (endTime - startTime)/1000.0 + " seconds");
-
-        startTime = System.currentTimeMillis();
-        adjustCurrentDependencies(current, currentByResourceType);
-        adjustCurrentConfigs(current, adjustCurrent, pending);
-        endTime = System.currentTimeMillis();
-
-        System.out.println("adjust current took " + (endTime - startTime)/1000.0 + " seconds");
-
-        try {
-            ResourceDiff resourceDiff = DiffUtil.generateDiff(beamProvider, null, adjustCurrent, pending);
-            //resolveReference(Arrays.asList(resourceDiff));
-            return resourceDiff;
-
-        } catch (Exception e) {
-            throw new BeamException(e.getMessage(), e.getCause());
         }
     }
 }
