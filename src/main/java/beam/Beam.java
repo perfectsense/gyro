4b56ef393313e4195a5eacc543e055c27a4b90d7
Hao Sun <PSDhao@users.noreply.github.com>
2018-11-09 16:03:25 -0500

    Add example usage

--- src/main/java/beam/Beam.java
+++ src/main/java/beam/Beam.java
@@ -1,25 +1,19 @@
 package beam;
 
-import beam.commands.SyncCommand;
-import beam.commands.UpCommand;
-import io.airlift.airline.Cli;
-import io.airlift.airline.Help;
+import beam.lang.BCL;
+import beam.lang.BeamConfig;
 
 public class Beam {
 
     public static void main(String[] arguments) throws Exception {
-        Cli.CliBuilder<Object> builder = Cli.builder("beam").
-                withDescription("Beam.").
-                withDefaultCommand(Help.class).
-                withCommand(UpCommand.class)
-                .withCommand(SyncCommand.class);
+        try {
+            BCL.init();
+            BeamConfig root = BCL.parse("example.bcl");
+            BCL.resolve(root);
 
-        Cli<Object> cli = builder.build();
-
-        Object command = cli.parse(arguments);
-        if (command instanceof Runnable) {
-            ((Runnable) command).run();
+            System.out.println(root);
+        } finally {
+            BCL.shutdown();
         }
     }
-
 }
