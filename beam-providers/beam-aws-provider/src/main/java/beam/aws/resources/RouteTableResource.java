888f4cedea501e2f23a14c06f749f7fc773ac8de
Jeremy Collins <jeremy@perfectsensedigital.com>
2018-12-05 19:52:44 -0500

    Implement route table resource

---
+++ beam-providers/beam-aws-provider/src/main/java/beam/aws/resources/RouteTableResource.java
@@ -0,0 +1,106 @@
+package beam.aws.resources;
+
+import beam.core.BeamCredentials;
+import beam.core.diff.ResourceName;
+import beam.lang.BeamContextKey;
+import beam.lang.BeamLiteral;
+import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.model.CreateRouteTableResponse;
+import software.amazon.awssdk.services.ec2.model.DescribeRouteTablesResponse;
+import software.amazon.awssdk.services.ec2.model.Filter;
+import software.amazon.awssdk.services.ec2.model.RouteTable;
+
+import java.util.Set;
+
+@ResourceName("route-table")
+public class RouteTableResource extends TaggableResource<RouteTable> {
+
+    private String routeTableId;
+    private String ownerId;
+    private String vpcId;
+
+    public String getRouteTableId() {
+        return routeTableId;
+    }
+
+    public void setRouteTableId(String routeTableId) {
+        this.routeTableId = routeTableId;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getVpcId() {
+        return vpcId;
+    }
+
+    public void setVpcId(String vpcId) {
+        this.vpcId = vpcId;
+    }
+
+    @Override
+    public void refresh(BeamCredentials cloud) {
+        Ec2Client client = createClient(Ec2Client.class);
+
+        DescribeRouteTablesResponse response = client.describeRouteTables(r -> r.filters(
+                Filter.builder().name("route-table-id").values(getRouteTableId()).build()
+        ));
+
+        for (RouteTable routeTable : response.routeTables()) {
+            setVpcId(routeTable.vpcId());
+            setOwnerId(routeTable.ownerId());
+
+            return;
+        }
+    }
+
+    @Override
+    protected String getId() {
+        return getRouteTableId();
+    }
+
+    @Override
+    protected void doCreate() {
+        Ec2Client client = createClient(Ec2Client.class);
+
+        CreateRouteTableResponse response = client.createRouteTable(r -> r.vpcId(getVpcId()));
+
+        setRouteTableId(response.routeTable().routeTableId());
+        setOwnerId(response.routeTable().ownerId());
+
+        addReferable(new BeamContextKey("route-table-id"), new BeamLiteral(getRouteTableId()));
+    }
+
+    @Override
+    protected void doUpdate(AwsResource config, Set<String> changedProperties) {
+
+    }
+
+    @Override
+    public void delete() {
+        Ec2Client client = createClient(Ec2Client.class);
+
+        client.deleteRouteTable(r -> r.routeTableId(getRouteTableId()));
+    }
+
+    @Override
+    public String toDisplayString() {
+        StringBuilder sb = new StringBuilder();
+        String routeTableId = getRouteTableId();
+
+        if (routeTableId != null) {
+            sb.append(routeTableId);
+
+        } else {
+            sb.append("route table");
+        }
+
+        return sb.toString();
+    }
+
+}
