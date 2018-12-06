98d288d0ccb11996196fad2804adfffe686bf511
Jeremy Collins <jeremy@perfectsensedigital.com>
2018-12-05 20:52:36 -0500

    Implement internet gateway resource

---
+++ beam-providers/beam-aws-provider/src/main/java/beam/aws/resources/InternetGatewayResource.java
@@ -0,0 +1,111 @@
+package beam.aws.resources;
+
+import beam.core.BeamCredentials;
+import beam.core.diff.ResourceName;
+import beam.lang.BeamContextKey;
+import beam.lang.BeamLiteral;
+import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.model.AttachInternetGatewayResponse;
+import software.amazon.awssdk.services.ec2.model.CreateInternetGatewayResponse;
+import software.amazon.awssdk.services.ec2.model.DescribeInternetGatewaysResponse;
+import software.amazon.awssdk.services.ec2.model.InternetGateway;
+import software.amazon.awssdk.services.ec2.model.InternetGatewayAttachment;
+
+import java.util.Set;
+
+@ResourceName("internet-gateway")
+public class InternetGatewayResource extends TaggableResource<InternetGateway> {
+
+    private String internetGatewayId;
+    private String vpcId;
+
+    public String getInternetGatewayId() {
+        return internetGatewayId;
+    }
+
+    public void setInternetGatewayId(String internetGatewayId) {
+        this.internetGatewayId = internetGatewayId;
+    }
+
+    public String getVpcId() {
+        return vpcId;
+    }
+
+    public void setVpcId(String vpcId) {
+        this.vpcId = vpcId;
+    }
+
+    @Override
+    protected String getId() {
+        return getVpcId();
+    }
+
+    @Override
+    public void refresh(BeamCredentials cloud) {
+        Ec2Client client = createClient(Ec2Client.class);
+
+        DescribeInternetGatewaysResponse response = client.describeInternetGateways(
+                r -> r.internetGatewayIds(getInternetGatewayId())
+        );
+
+        for (InternetGateway gateway : response.internetGateways()) {
+            for (InternetGatewayAttachment attachment : gateway.attachments()) {
+                setVpcId(attachment.vpcId());
+
+                break;
+            }
+        }
+    }
+
+    @Override
+    protected void doCreate() {
+        Ec2Client client = createClient(Ec2Client.class);
+
+        CreateInternetGatewayResponse response = client.createInternetGateway();
+
+        setInternetGatewayId(response.internetGateway().internetGatewayId());
+        addReferable(new BeamContextKey("internet-gateway-id"), new BeamLiteral(getInternetGatewayId()));
+
+        if (getVpcId() != null) {
+            client.attachInternetGateway(r -> r.internetGatewayId(getInternetGatewayId())
+                    .vpcId(getVpcId())
+            );
+        }
+    }
+
+    @Override
+    protected void doUpdate(AwsResource config, Set<String> changedProperties) {
+
+    }
+
+    @Override
+    public void delete() {
+        Ec2Client client = createClient(Ec2Client.class);
+
+        for (InternetGateway gateway : client.describeInternetGateways(
+                r -> r.internetGatewayIds(getInternetGatewayId())).internetGateways()) {
+           for (InternetGatewayAttachment attachment : gateway.attachments()) {
+               client.detachInternetGateway(
+                       r -> r.internetGatewayId(getInternetGatewayId()).vpcId(attachment.vpcId())
+               );
+           }
+        }
+
+        client.deleteInternetGateway(r -> r.internetGatewayId(getInternetGatewayId()));
+    }
+
+    @Override
+    public String toDisplayString() {
+        StringBuilder sb = new StringBuilder();
+
+        if (getInternetGatewayId() != null) {
+            sb.append(getInternetGatewayId());
+
+        } else {
+            sb.append("internet gateway");
+        }
+
+        return sb.toString();
+    }
+
+}
