358973b1b50052e914de28141dafc42f80cf26d5
Hao Sun <PSDhao@users.noreply.github.com>
2018-12-10 17:57:31 -0500

    Update message to show the config type

--- beam-core/src/main/java/beam/core/BeamValidatedConfig.java
+++ beam-core/src/main/java/beam/core/BeamValidatedConfig.java
@@ -27,7 +27,7 @@ public class BeamValidatedConfig extends BeamConfig {
                     if (referable instanceof BeamValue) {
                         BeamValue beamValue = (BeamValue) referable;
                         if (beamValue.getLine() != null) {
-                            throw new BeamException(String.format("resource '%s' at line %s => %s is not a valid field.", id, beamValue.getLine(), key.getId()));
+                            throw new BeamException(String.format("%s '%s' at line %s => %s is not a valid field.", getType(), id, beamValue.getLine(), key.getId()));
                         }
                     }
                 }
