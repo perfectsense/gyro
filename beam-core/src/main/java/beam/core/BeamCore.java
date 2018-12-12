b83dcf1f214b1fda8b33f68fff8d59c03642b0c0
Hao Sun <PSDhao@users.noreply.github.com>
2018-12-12 15:10:16 -0500

    Move Beam ui to core

--- beam-core/src/main/java/beam/core/BeamCore.java
+++ beam-core/src/main/java/beam/core/BeamCore.java
@@ -2,14 +2,29 @@ package beam.core;
 
 import beam.lang.BCL;
 import beam.lang.BeamConfig;
+import com.psddev.dari.util.ThreadLocalStack;
 
 import java.io.IOException;
 
 public class BeamCore {
 
-    private final BCL lang = new BCL();
+    private static final ThreadLocalStack<BeamUI> UI = new ThreadLocalStack<>();
 
     private static final BeamValidationException validationException = new BeamValidationException("Invalid config!");
+
+    private final BCL lang = new BCL();
+
+    public static BeamUI ui() {
+        return UI.get();
+    }
+
+    public static void pushUi(BeamUI ui) {
+        UI.push(ui);
+    }
+
+    public static BeamUI popUi() {
+        return UI.pop();
+    }
 
     public static BeamValidationException validationException() {
         return validationException;
