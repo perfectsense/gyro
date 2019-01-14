371a122c7b795a3da802a59afba15f44049ced0a
Jeremy Collins <jeremy@perfectsensedigital.com>
2019-01-14 14:47:40 -0500

    Remove unused imports

--- beam-core/src/main/java/beam/lang/ResourceContainer.java
+++ beam-core/src/main/java/beam/lang/ResourceContainer.java
@@ -1,9 +1,7 @@
 package beam.lang;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class ResourceContainer extends Container {
