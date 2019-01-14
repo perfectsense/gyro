2c60f336f8a7210e79ce5d8afc65a3d9758f1033
Renee Adkins <reneeskate@aol.com>
2019-01-14 12:46:13 -0500

    Add initial variables along with their getters and setters

---
+++ beam-providers/beam-aws-provider/src/main/java/beam/aws/elb/ElasticLoadBalancerResource.java
@@ -0,0 +1,98 @@
+package beam.aws.elb;
+
+import beam.aws.AwsResource;
+import beam.core.diff.ResourceDiffProperty;
+import beam.core.diff.ResourceName;
+import beam.lang.Resource;
+
+import software.amazon.awssdk.services.elasticloadbalancing.ElasticLoadBalancingClient;
+import software.amazon.awssdk.services.elasticloadbalancing.model.CreateLoadBalancerResponse;
+import software.amazon.awssdk.services.elasticloadbalancing.model.Listener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * Example
+ * -------
+ *
+ * .. code-block:: beam
+ *
+ *     aws::elastic-load-balancer elb-example
+ *
+ *     end
+ */
+@ResourceName("elastic-load-balancer")
+public class ElasticLoadBalancerResource extends AwsResource {
+
+    private String loadBalancerName;
+    private List<ListenerResource> listeners;
+    private List<String> securityGroups;
+    private List<String> subnets;
+
+    public List<ListenerResource> getListeners() {
+        if (listeners == null) {
+            listeners = new ArrayList<>();
+        }
+
+        return listeners;
+    }
+
+    public void setListeners(List<ListenerResource> listeners) {
+        this.listeners = listeners;
+    }
+
+    public String getLoadBalancerName() {
+        return loadBalancerName;
+    }
+
+    public void setLoadBalancerName(String loadBalancerName) {
+        this.loadBalancerName = loadBalancerName;
+    }
+
+    public List<String> getSecurityGroups() {
+        if (securityGroups == null) {
+            securityGroups = new ArrayList<>();
+        }
+
+        return securityGroups;
+    }
+
+    public void setSecurityGroups(List<String> securityGroups) {
+        this.securityGroups = securityGroups;
+    }
+
+    public List<String> getSubnets() {
+        if (subnets == null) {
+            subnets = new ArrayList<>();
+        }
+
+        return subnets;
+    }
+
+    public void setSubnets(List<String> subnets) {
+        this.subnets = subnets;
+    }
+
+    @Override
+    public boolean refresh() {return false;}
+
+    @Override
+    public void create() {}
+
+    @Override
+    public void update(Resource current, Set<String> changedProperties) {}
+
+    @Override
+    public void delete() {}
+
+    @Override
+    public String toDisplayString() {
+        StringBuilder sb = new StringBuilder();
+        return sb.toString();
+    }
+
+
+}
