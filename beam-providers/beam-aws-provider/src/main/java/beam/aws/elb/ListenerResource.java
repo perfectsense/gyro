bcaa7912f9cdc3bccc47d7cf999a4838e27ae764
Renee Adkins <reneeskate@aol.com>
2019-01-16 14:26:09 -0500

    Add comments and debugging print statements

--- beam-providers/beam-aws-provider/src/main/java/beam/aws/elb/ListenerResource.java
+++ beam-providers/beam-aws-provider/src/main/java/beam/aws/elb/ListenerResource.java
@@ -1,8 +1,12 @@
 package beam.aws.elb;
 
 import beam.aws.AwsResource;
+import beam.core.diff.ChangeType;
+import beam.core.diff.ResourceDiffProperty;
 import beam.core.diff.ResourceName;
+
 import beam.lang.Resource;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.elasticloadbalancing.ElasticLoadBalancingClient;
 import software.amazon.awssdk.services.elasticloadbalancing.model.Listener;
 
@@ -26,7 +30,7 @@ import java.util.Set;
  *     end
  */
 
-@ResourceName(parent = "load-balancer-resource", value = "listener")
+@ResourceName(parent = "load-balancer", value = "listener")
 public class ListenerResource extends AwsResource {
 
     private Integer instancePort;
@@ -35,63 +39,88 @@ public class ListenerResource extends AwsResource {
     private String protocol;
     private String sslCertificateId;
 
+    @ResourceDiffProperty(updatable = true)
     public Integer getInstancePort() {
         return instancePort;
     }
 
     public void setInstancePort(Integer instancePort) { this.instancePort = instancePort; }
 
+    @ResourceDiffProperty(updatable = true)
     public String getInstanceProtocol() {
         return instanceProtocol;
     }
 
     public void setInstanceProtocol(String instanceProtocol) { this.instanceProtocol = instanceProtocol; }
 
+    @ResourceDiffProperty(updatable = true)
     public Integer getLoadBalancerPort() {
         return loadBalancerPort;
     }
 
     public void setLoadBalancerPort(Integer loadBalancerPort) { this.loadBalancerPort = loadBalancerPort; }
 
+    @ResourceDiffProperty(updatable = true)
     public String getProtocol() {
         return protocol;
     }
 
     public void setProtocol(String protocol) { this.protocol = protocol; }
-
+    /*
+    @ResourceDiffProperty(updatable = true)
     public String getSslCertificateId() { return sslCertificateId; }
 
     public void setSslCertificateId(String sslCertificateId) { this.sslCertificateId = sslCertificateId; }
-    /*
+    */
     public String getLoadBalancer() {
-        LoadBalancerResource parent = (LoadBalancerResource) parentResourceNode();
+        LoadBalancerResource parent = (LoadBalancerResource) parentResource();
+        System.out.println("Is parent null "+(parent == null));
+
         if (parent != null) {
+            System.out.println("Load balancer name "+parent.getLoadBalancerName());
             return parent.getLoadBalancerName();
         }
 
         return null;
-    }*/
+    }
 
     @Override
-    public boolean refresh() {return false;}
+    public boolean refresh() {
+        // parent must refresh because of the listeners list
+        LoadBalancerResource parent = (LoadBalancerResource) parentResource();
+        return parent.refresh();
+    }
 
     @Override
-    public void create() {}
+    public void create() {
+        // all listeners are created in the parent
+        if (parentResource().change().getType() == ChangeType.CREATE) {
+            return;
+        }
+    }
 
     @Override
     public void update(Resource current, Set<String> changedProperties) {
-        //remove and attach based on new list of listeners
-        //LoadBalancerResource parent = (LoadBalancerResource) parentResourceNode();
-        //parent.update();
+        // parent must refresh because of the listeners list
+        // call parent's update function
 
     }
 
     @Override
     public void delete() {
-        ElasticLoadBalancingClient client = ElasticLoadBalancingClient.builder()
-                .build();
+        // if parent is going to be deleted, then the listeners will
+        // get deleted
+        /*
+        if (parentResource().change().getType() == ChangeType.DELETE) {
+            return;
+        }*/
 
-        //client.deleteLoadBalancerListeners(r -> r.loadBalancerName(getLoadBalancer()));
+        ElasticLoadBalancingClient client = ElasticLoadBalancingClient.builder()
+                .region(Region.US_EAST_1)
+                .build();
+        System.out.println("Get load balancer name "+getLoadBalancer());
+        client.deleteLoadBalancerListeners(r -> r.loadBalancerName(getLoadBalancer())
+                                                .loadBalancerPorts(getLoadBalancerPort()));
     }
 
     @Override
