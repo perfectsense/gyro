8e751b8168f6d96cdd3f5f36687451c12350ad7b
Renee Adkins <reneeskate@aol.com>
2019-01-16 15:48:55 -0500

    Final pass before testing

--- beam-providers/beam-aws-provider/src/main/java/beam/aws/elb/ListenerResource.java
+++ beam-providers/beam-aws-provider/src/main/java/beam/aws/elb/ListenerResource.java
@@ -6,8 +6,8 @@ import beam.core.diff.ResourceDiffProperty;
 import beam.core.diff.ResourceName;
 
 import beam.lang.Resource;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.elasticloadbalancing.ElasticLoadBalancingClient;
+import software.amazon.awssdk.services.elasticloadbalancing.model.Instance;
 import software.amazon.awssdk.services.elasticloadbalancing.model.Listener;
 
 import java.util.ArrayList;
@@ -84,40 +84,54 @@ public class ListenerResource extends AwsResource {
         return null;
     }
 
+    public Listener toListener() {
+            Listener newListener = Listener.builder()
+                    .instancePort(getInstancePort())
+                    .instanceProtocol(getInstanceProtocol())
+                    .loadBalancerPort(getLoadBalancerPort())
+                    .protocol(getProtocol())
+                    //.sslCertificateId(resource.getSslCertificateId())
+                    .build();
+
+        return newListener;
+    }
+
+
     @Override
     public boolean refresh() {
-        // parent must refresh because of the listeners list
-        LoadBalancerResource parent = (LoadBalancerResource) parentResource();
-        return parent.refresh();
+        return true;
     }
 
     @Override
     public void create() {
-        // all listeners are created in the parent
         if (parentResource().change().getType() == ChangeType.CREATE) {
             return;
         }
+
+        ElasticLoadBalancingClient client = createClient(ElasticLoadBalancingClient.class);
+        client.createLoadBalancerListeners(r -> r.listeners(toListener()));
+
     }
 
     @Override
     public void update(Resource current, Set<String> changedProperties) {
         // parent must refresh because of the listeners list
         // call parent's update function
-
+        // delete and create
+        delete();
+        create();
     }
 
     @Override
     public void delete() {
         // if parent is going to be deleted, then the listeners will
         // get deleted
-        /*
+
         if (parentResource().change().getType() == ChangeType.DELETE) {
             return;
-        }*/
+        }
+        ElasticLoadBalancingClient client = createClient(ElasticLoadBalancingClient.class);
 
-        ElasticLoadBalancingClient client = ElasticLoadBalancingClient.builder()
-                .region(Region.US_EAST_1)
-                .build();
         System.out.println("Get load balancer name "+getLoadBalancer());
         client.deleteLoadBalancerListeners(r -> r.loadBalancerName(getLoadBalancer())
                                                 .loadBalancerPorts(getLoadBalancerPort()));
