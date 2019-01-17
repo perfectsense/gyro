4c9cb21b0fe5f0a1503e1b948429c1a5e87e7d22
Jeremy Collins <jeremy@perfectsensedigital.com>
2019-01-17 07:25:50 -0500

    Fix BeamDoclet, base Resource class name changed

--- beam-docs/src/main/java/beam/docs/BeamDoclet.java
+++ beam-docs/src/main/java/beam/docs/BeamDoclet.java
@@ -55,8 +55,8 @@ public class BeamDoclet implements Doclet {
     @Override
     public boolean run(DocletEnvironment environment) {
         TypeMirror beamResource = null;
-        for (TypeElement classElement : environment.getElementUtils().getAllTypeElements("beam.core.BeamResource")) {
-            if ("beam.core.BeamResource".equals(classElement.getQualifiedName().toString())) {
+        for (TypeElement classElement : environment.getElementUtils().getAllTypeElements("beam.lang.Resource")) {
+            if ("beam.lang.Resource".equals(classElement.getQualifiedName().toString())) {
                 beamResource = classElement.asType();
             }
         }
