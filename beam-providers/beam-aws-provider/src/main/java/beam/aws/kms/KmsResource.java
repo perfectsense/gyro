c827e41a12ae6b2dfff948e8df9f068e41de28ce
Renee Adkins <reneeskate@aol.com>
2019-02-06 22:13:32 -0500

    Add necessary imports

--- beam-providers/beam-aws-provider/src/main/java/beam/aws/kms/KmsResource.java
+++ beam-providers/beam-aws-provider/src/main/java/beam/aws/kms/KmsResource.java
@@ -1,2 +1,28 @@
 package beam.aws.kms;
+
+import beam.aws.AwsResource;
+import beam.core.BeamException;
+import beam.core.diff.ResourceDiffProperty;
+import beam.core.diff.ResourceName;
+import beam.lang.Resource;
+import com.psddev.dari.util.CompactMap;
+
+import software.amazon.awssdk.services.kms.KmsClient;
+import software.amazon.awssdk.services.kms.model.AliasListEntry;
+import software.amazon.awssdk.services.kms.model.CreateKeyResponse;
+import software.amazon.awssdk.services.kms.model.DescribeKeyResponse;
+import software.amazon.awssdk.services.kms.model.GetKeyPolicyResponse;
+import software.amazon.awssdk.services.kms.model.KeyMetadata;
+import software.amazon.awssdk.services.kms.model.ListAliasesResponse;
+import software.amazon.awssdk.services.kms.model.Tag;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
 @ResourceName("kms")
