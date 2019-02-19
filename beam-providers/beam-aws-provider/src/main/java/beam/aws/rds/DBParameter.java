55f77aa86ea2d4b9f077bec8e18fdd1d9f8fd123
Hao Sun <PSDhao@users.noreply.github.com>
2019-02-19 13:18:53 -0500

    Add db parameter

---
+++ beam-providers/beam-aws-provider/src/main/java/beam/aws/rds/DBParameter.java
@@ -0,0 +1,64 @@
+package beam.aws.rds;
+
+import beam.core.diff.Diffable;
+import beam.core.diff.ResourceDiffProperty;
+
+public class DBParameter extends Diffable {
+
+    private String name;
+    private String value;
+    private String applyMethod;
+
+    /**
+     * The name of the DB parameter. (Required)
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * The value of the DB parameter. (Required)
+     */
+    @ResourceDiffProperty(updatable = true)
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    /**
+     * When to apply parameter updates ("immediate" (default) or "pending-reboot").
+     */
+    public String getApplyMethod() {
+        if (applyMethod == null) {
+            applyMethod = "immediate";
+        }
+
+        return applyMethod;
+    }
+
+    public void setApplyMethod(String applyMethod) {
+        this.applyMethod = applyMethod;
+    }
+
+    @Override
+    public String primaryKey() {
+        return getName();
+    }
+
+    @Override
+    public String toDisplayString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("db parameter group ");
+        sb.append(getName());
+
+        return sb.toString();
+    }
+}
