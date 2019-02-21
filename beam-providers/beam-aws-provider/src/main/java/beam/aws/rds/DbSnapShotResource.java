d016cbf37e13f5c9d3117a211bf52103ed958581
Hao Sun <PSDhao@users.noreply.github.com>
2019-02-20 21:00:57 -0500

    Add db snapshot resource

---
+++ beam-providers/beam-aws-provider/src/main/java/beam/aws/rds/DbSnapShotResource.java
@@ -0,0 +1,119 @@
+package beam.aws.rds;
+
+import beam.core.BeamException;
+import beam.core.diff.ResourceDiffProperty;
+import beam.core.diff.ResourceName;
+import beam.lang.Resource;
+import com.psddev.dari.util.ObjectUtils;
+import software.amazon.awssdk.services.rds.RdsClient;
+import software.amazon.awssdk.services.rds.model.CreateDbSnapshotResponse;
+import software.amazon.awssdk.services.rds.model.DbSnapshotNotFoundException;
+import software.amazon.awssdk.services.rds.model.DescribeDbSnapshotsResponse;
+
+import java.util.Set;
+
+@ResourceName("db-snapshot")
+public class DbSnapShotResource extends RdsTaggableResource {
+
+    private String dbInstanceIdentifier;
+    private String dbSnapshotIdentifier;
+    private String engineVersion;
+    private String optionGroupName;
+
+    public String getDbInstanceIdentifier() {
+        return dbInstanceIdentifier;
+    }
+
+    public void setDbInstanceIdentifier(String dbInstanceIdentifier) {
+        this.dbInstanceIdentifier = dbInstanceIdentifier;
+    }
+
+    public String getDbSnapshotIdentifier() {
+        return dbSnapshotIdentifier;
+    }
+
+    public void setDbSnapshotIdentifier(String dbSnapshotIdentifier) {
+        this.dbSnapshotIdentifier = dbSnapshotIdentifier;
+    }
+
+    @ResourceDiffProperty(updatable = true)
+    public String getEngineVersion() {
+        return engineVersion;
+    }
+
+    public void setEngineVersion(String engineVersion) {
+        this.engineVersion = engineVersion;
+    }
+
+    @ResourceDiffProperty(updatable = true)
+    public String getOptionGroupName() {
+        return optionGroupName;
+    }
+
+    public void setOptionGroupName(String optionGroupName) {
+        this.optionGroupName = optionGroupName;
+    }
+
+    @Override
+    protected boolean doRefresh() {
+        RdsClient client = createClient(RdsClient.class);
+
+        if (ObjectUtils.isBlank(getDbSnapshotIdentifier())) {
+            throw new BeamException("db-snapshot-identifier is missing, unable to load db snapshot.");
+        }
+
+        try {
+            DescribeDbSnapshotsResponse response = client.describeDBSnapshots(
+                r -> r.dbSnapshotIdentifier(getDbSnapshotIdentifier())
+            );
+
+            response.dbSnapshots().stream()
+                .forEach(s -> {
+                        setDbInstanceIdentifier(s.dbInstanceIdentifier());
+                        setEngineVersion(s.engineVersion());
+                        setOptionGroupName(s.optionGroupName());
+                        setArn(s.dbSnapshotArn());
+                    }
+                );
+
+        } catch (DbSnapshotNotFoundException ex) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    protected void doCreate() {
+        RdsClient client = createClient(RdsClient.class);
+        CreateDbSnapshotResponse response = client.createDBSnapshot(
+            r -> r.dbInstanceIdentifier(getDbInstanceIdentifier())
+                    .dbSnapshotIdentifier(getDbSnapshotIdentifier())
+        );
+
+        setArn(response.dbSnapshot().dbSnapshotArn());
+    }
+
+    @Override
+    protected void doUpdate(Resource config, Set<String> changedProperties) {
+        RdsClient client = createClient(RdsClient.class);
+        client.modifyDBSnapshot(
+            r -> r.dbSnapshotIdentifier(getDbSnapshotIdentifier())
+                    .engineVersion(getEngineVersion())
+                    .optionGroupName(getOptionGroupName())
+        );
+    }
+
+    @Override
+    public void delete() {
+        RdsClient client = createClient(RdsClient.class);
+        client.deleteDBSnapshot(
+            r -> r.dbSnapshotIdentifier(getDbSnapshotIdentifier())
+        );
+    }
+
+    @Override
+    public String toDisplayString() {
+        return "db snapshot " + getDbSnapshotIdentifier();
+    }
+}
