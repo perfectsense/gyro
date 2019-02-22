6223988c8dd713be303bbe3545706f85fbebd4f1
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-02-22 10:15:13 -0500

    Update public ip address.

--- beam-providers/beam-azure-provider/src/main/java/beam/azure/network/PublicIpAddressResource.java
+++ beam-providers/beam-azure-provider/src/main/java/beam/azure/network/PublicIpAddressResource.java
@@ -1,14 +1,19 @@
 package beam.azure.network;
 
 import beam.azure.AzureResource;
+import beam.core.diff.ResourceDiffProperty;
 import beam.core.diff.ResourceName;
 import beam.lang.Resource;
 import com.microsoft.azure.management.Azure;
 import com.microsoft.azure.management.network.PublicIPAddress;
+import com.microsoft.azure.management.network.PublicIPAddress.DefinitionStages.WithCreate;
 import com.microsoft.azure.management.network.PublicIPSkuType;
+import com.microsoft.azure.management.resources.fluentcore.arm.AvailabilityZoneId;
 import com.microsoft.azure.management.resources.fluentcore.arm.Region;
 import com.psddev.dari.util.ObjectUtils;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 @ResourceName("public-ip-address")
@@ -20,6 +25,9 @@ public class PublicIpAddressResource extends AzureResource {
     private Integer idleTimeoutInMinute;
     private String publicIpAddressId;
     private String ipAddress;
+    private String availabilityZoneId;
+    private String domainLabel;
+    private Map<String, String> tags;
 
     public String getName() {
         return name;
@@ -61,6 +69,7 @@ public class PublicIpAddressResource extends AzureResource {
         this.dynamic = dynamic;
     }
 
+    @ResourceDiffProperty(updatable = true)
     public Integer getIdleTimeoutInMinute() {
         return idleTimeoutInMinute;
     }
@@ -85,6 +94,36 @@ public class PublicIpAddressResource extends AzureResource {
         this.ipAddress = ipAddress;
     }
 
+    public String getAvailabilityZoneId() {
+        return availabilityZoneId;
+    }
+
+    public void setAvailabilityZoneId(String availabilityZoneId) {
+        this.availabilityZoneId = availabilityZoneId;
+    }
+
+    @ResourceDiffProperty(updatable = true)
+    public String getDomainLabel() {
+        return domainLabel;
+    }
+
+    public void setDomainLabel(String domainLabel) {
+        this.domainLabel = domainLabel;
+    }
+
+    @ResourceDiffProperty(updatable = true)
+    public Map<String, String> getTags() {
+        if (tags == null) {
+            tags = new HashMap<>();
+        }
+
+        return tags;
+    }
+
+    public void setTags(Map<String, String> tags) {
+        this.tags = tags;
+    }
+
     @Override
     public boolean refresh() {
         Azure client = createClient();
@@ -92,6 +131,9 @@ public class PublicIpAddressResource extends AzureResource {
         PublicIPAddress publicIpAddress = client.publicIPAddresses().getByResourceGroup(getResourceGroupName(), getName());
 
         setIpAddress(publicIpAddress.ipAddress());
+        setDomainLabel(publicIpAddress.leafDomainLabel());
+        setIdleTimeoutInMinute(publicIpAddress.idleTimeoutInMinutes());
+        setTags(publicIpAddress.tags());
 
         return true;
     }
@@ -100,15 +142,38 @@ public class PublicIpAddressResource extends AzureResource {
     public void create() {
         Azure client = createClient();
 
-        PublicIPAddress publicIpAddress = client.publicIPAddresses()
+        WithCreate withCreate = client.publicIPAddresses()
             .define(getName())
             .withRegion(Region.fromName(getRegion()))
             .withExistingResourceGroup(getResourceGroupName())
-            .withSku(getSkuBasic() ? PublicIPSkuType.BASIC : PublicIPSkuType.STANDARD)
-            .withDynamicIP()
-            .withIdleTimeoutInMinutes(getIdleTimeoutInMinute())
-            .withoutLeafDomainLabel()
-            .create();
+            .withSku(getSkuBasic() ? PublicIPSkuType.BASIC : PublicIPSkuType.STANDARD);
+
+        if (!ObjectUtils.isBlank(getAvailabilityZoneId())) {
+            withCreate = withCreate.withAvailabilityZone(AvailabilityZoneId.fromString(getAvailabilityZoneId()));
+        }
+
+        if (getSkuBasic()) {
+            //basic
+            withCreate = withCreate.withIdleTimeoutInMinutes(getIdleTimeoutInMinute());
+
+            if (!ObjectUtils.isBlank(getDomainLabel())) {
+                withCreate = withCreate.withLeafDomainLabel(getDomainLabel());
+            } else {
+                withCreate = withCreate.withoutLeafDomainLabel();
+            }
+        } else {
+            //standard
+            withCreate = withCreate.withStaticIP()
+                .withIdleTimeoutInMinutes(getIdleTimeoutInMinute());
+
+            if (!ObjectUtils.isBlank(getDomainLabel())) {
+                withCreate = withCreate.withLeafDomainLabel(getDomainLabel());
+            } else {
+                withCreate = withCreate.withoutLeafDomainLabel();
+            }
+        }
+
+        PublicIPAddress publicIpAddress = withCreate.withTags(getTags()).create();
 
         setPublicIpAddressId(publicIpAddress.id());
         setIpAddress(publicIpAddress.ipAddress());
@@ -116,7 +181,28 @@ public class PublicIpAddressResource extends AzureResource {
 
     @Override
     public void update(Resource current, Set<String> changedProperties) {
+        Azure client = createClient();
 
+        PublicIPAddress publicIPAddress = client.publicIPAddresses().getByResourceGroup(getResourceGroupName(), getName());
+
+        PublicIPAddress.Update update = publicIPAddress.update();
+
+        if (changedProperties.contains("idle-timeout-in-minute")) {
+            update = update.withIdleTimeoutInMinutes(getIdleTimeoutInMinute());
+        }
+
+        if (changedProperties.contains("tags")) {
+            update = update.withTags(getTags());
+        }
+
+        if (changedProperties.contains("domain-label")) {
+            update = ObjectUtils.isBlank(getDomainLabel())
+                ? update.withoutLeafDomainLabel() : update.withLeafDomainLabel(getDomainLabel());
+        }
+
+        if (!changedProperties.isEmpty()) {
+            update.apply();
+        }
     }
 
     @Override
