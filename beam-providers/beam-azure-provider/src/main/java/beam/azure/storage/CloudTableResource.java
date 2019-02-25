3b1ffc2f10b7907bf052d42eda8ae1b548f02694
Renee Adkins <reneeskate@aol.com>
2019-02-24 19:21:59 -0500

    Implement cloud table resource

---
+++ beam-providers/beam-azure-provider/src/main/java/beam/azure/storage/CloudTableResource.java
@@ -0,0 +1,106 @@
+package beam.azure.storage;
+
+import beam.azure.AzureResource;
+import beam.core.BeamException;
+import beam.core.diff.ResourceName;
+import beam.lang.Resource;
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.StorageException;
+
+import com.microsoft.azure.storage.table.CloudTable;
+import com.microsoft.azure.storage.table.CloudTableClient;
+
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
+import java.util.Set;
+
+/**
+ * Creates a table
+ *
+ * Example
+ * -------
+ *
+ * .. code-block:: beam
+ *
+ *     azure::cloud-table cloud-table-example
+ *         cloud-table-name: "cloudtablename"
+ *         storage-connection: $(azure::storage-account queue-storage-account-example | storage-connection)
+ *     end
+ */
+@ResourceName("cloud-table")
+public class CloudTableResource extends AzureResource {
+
+    private String cloudTableName;
+    private String storageConnection;
+
+    /**
+     * The name of the table (Required)
+     */
+    public String getCloudTableName() {
+        return cloudTableName;
+    }
+
+    public void setCloudTableName(String cloudTableName) {
+        this.cloudTableName = cloudTableName;
+    }
+
+    public String getStorageConnection() {
+        return storageConnection;
+    }
+
+    public void setStorageConnection(String storageConnection) {
+        this.storageConnection = storageConnection;
+    }
+
+    @Override
+    public boolean refresh() {
+
+        try {
+            CloudTable cloudTable = cloudTable();
+            if (cloudTable.exists()) {
+                setCloudTableName(cloudTable.getName());
+                return true;
+            }
+
+            return false;
+        } catch (StorageException ex) {
+            throw new BeamException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void create() {
+        try {
+            CloudTable cloudTable = cloudTable();
+            cloudTable.create();
+        } catch (StorageException ex) {
+            throw new BeamException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void update(Resource current, Set<String> changedProperties) {}
+
+    @Override
+    public void delete() {
+        try {
+            CloudTable cloudTable = cloudTable();
+            cloudTable.delete();
+        } catch (StorageException ex) {
+            throw new BeamException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public String toDisplayString() {return "cloud table " + getCloudTableName(); }
+
+    private CloudTable cloudTable() {
+        try {
+            CloudStorageAccount account = CloudStorageAccount.parse(getStorageConnection());
+            CloudTableClient client = account.createCloudTableClient();
+            return client.getTableReference(getCloudTableName());
+        } catch (StorageException | URISyntaxException | InvalidKeyException ex) {
+            throw new BeamException(ex.getMessage());
+        }
+    }
+}
