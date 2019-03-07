32662416b62f61c4f6a85784d237e5da5fe4b278
Hao Sun <PSDhao@users.noreply.github.com>
2019-03-07 15:14:03 -0500

    Remove evaluate method from AbstractCompoundQuery

--- core/src/main/java/gyro/lang/ast/query/AbstractCompoundQuery.java
+++ core/src/main/java/gyro/lang/ast/query/AbstractCompoundQuery.java
@@ -1,10 +1,6 @@
 package gyro.lang.ast.query;
 
-import gyro.lang.ResourceQueryGroup;
-import gyro.lang.ast.scope.Scope;
 import gyro.parser.antlr4.BeamParser;
-
-import java.util.List;
 
 public abstract class AbstractCompoundQuery extends Query {
 
@@ -23,10 +19,4 @@ public abstract class AbstractCompoundQuery extends Query {
     public Query getRight() {
         return right;
     }
-
-    @Override
-    public List<ResourceQueryGroup> evaluate(Scope scope, String type, boolean external) throws Exception {
-        return null;
-    }
-
 }
