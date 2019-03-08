649cad81e09c84f5bdcc448955189f11faabccf5
Hao Sun <PSDhao@users.noreply.github.com>
2019-03-08 12:57:58 -0500

    Save stack trace to a log file instead of spit it out

--- core/src/main/java/gyro/core/BeamCore.java
+++ core/src/main/java/gyro/core/BeamCore.java
@@ -1,5 +1,6 @@
 package gyro.core;
 
+import com.psddev.dari.util.ObjectUtils;
 import com.psddev.dari.util.ThreadLocalStack;
 
 public class BeamCore {
@@ -18,4 +19,12 @@ public class BeamCore {
         return UI.pop();
     }
 
+    public static String getBeamUserHome() {
+        String userHome = System.getenv("BEAM_USER_HOME");
+        if (ObjectUtils.isBlank(userHome)) {
+            userHome = System.getProperty("user.home");
+        }
+
+        return userHome;
+    }
 }
