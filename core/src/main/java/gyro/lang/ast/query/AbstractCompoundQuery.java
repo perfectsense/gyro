f1d5c59ce97ba65ad9c9cb5c0571b853a8b8cfee
Hao Sun <PSDhao@users.noreply.github.com>
2019-03-11 14:34:12 -0400

    Flatten nested queries

--- core/src/main/java/gyro/lang/ast/query/AbstractCompoundQuery.java
+++ core/src/main/java/gyro/lang/ast/query/AbstractCompoundQuery.java
@@ -10,12 +10,21 @@ public abstract class AbstractCompoundQuery extends Query {
     private final List<Query> children;
 
     public AbstractCompoundQuery(QueryExpressionContext context) {
-        children = ImmutableList.of(
-            Query.create(context.getChild(0)),
-            Query.create(context.getChild(2)));
+        ImmutableList.Builder<Query> list = ImmutableList.builder();
+        addChildren(list, Query.create(context.getChild(0)));
+        addChildren(list, Query.create(context.getChild(2)));
+        children = list.build();
     }
 
     public List<Query> getChildren() {
         return children;
+    }
+
+    private void addChildren(ImmutableList.Builder<Query> list, Query child) {
+        if (getClass().isInstance(child)) {
+            list.addAll(((AbstractCompoundQuery) child).getChildren());
+        } else {
+            list.add(child);
+        }
     }
 }
