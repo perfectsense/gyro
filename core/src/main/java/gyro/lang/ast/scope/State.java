c036c37ee9cb58655628e1576712c9b591f9a7ae
Hyoo Lim <hyoolim@gmail.com>
2019-03-18 10:40:03 -0400

    Fixes replace during state not to trigger.
    This is because workflow does custom work during a replace, and if you keep this, it overwrites all changes done by the workflow.

--- core/src/main/java/gyro/lang/ast/scope/State.java
+++ core/src/main/java/gyro/lang/ast/scope/State.java
@@ -12,6 +12,7 @@ import gyro.core.diff.Delete;
 import gyro.core.diff.Diffable;
 import gyro.core.diff.DiffableField;
 import gyro.core.diff.DiffableType;
+import gyro.core.diff.Replace;
 import gyro.core.diff.ResourceName;
 import gyro.core.diff.ResourceNames;
 import gyro.lang.Resource;
@@ -75,6 +76,10 @@ public class State {
     }
 
     public void update(Change change) throws Exception {
+        if (change instanceof Replace) {
+            return;
+        }
+
         Diffable diffable = change.getDiffable();
 
         if (!(diffable instanceof Resource)) {
