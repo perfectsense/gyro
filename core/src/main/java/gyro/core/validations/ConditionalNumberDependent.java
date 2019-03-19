1bf684f8acf1f097a821baa181890f0d3ad23943
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-03-19 11:16:18 -0400

    Add conditional number validation.

--- core/src/main/java/gyro/core/validations/ConditionalNumberDependent.java
+++ core/src/main/java/gyro/core/validations/ConditionalNumberDependent.java
@@ -2,6 +2,7 @@ package gyro.core.validations;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -10,6 +11,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @AnnotationProcessorClass(ConditionalNumberDependentValidator.class)
+@Repeatable(value = ConditionalNumberDependents.class)
 public @interface ConditionalNumberDependent {
     String selected();
 
