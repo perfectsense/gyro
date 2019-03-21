4a91e1cb5dd77fb0447769a5f325b92c8bbca1fe
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-03-21 16:59:24 -0400

    Add repeatable to allowedNumberRange annotation.

--- core/src/main/java/gyro/core/validations/AllowedNumberRange.java
+++ core/src/main/java/gyro/core/validations/AllowedNumberRange.java
@@ -2,6 +2,7 @@ package gyro.core.validations;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -10,6 +11,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @AnnotationProcessorClass(AllowedNumberRangeValidator.class)
+@Repeatable(AllowedNumberRanges.class)
 public @interface AllowedNumberRange {
     double low();
 
