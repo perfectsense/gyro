76ba7a61e91bad77f263eb373b99d2611e9677d0
Hao Sun <PSDhao@users.noreply.github.com>
2019-03-21 13:27:45 -0400

    Throw an error if config file does not exist

--- core/src/main/java/gyro/commands/AbstractConfigCommand.java
+++ core/src/main/java/gyro/commands/AbstractConfigCommand.java
@@ -14,6 +14,7 @@ import com.psddev.dari.util.StringUtils;
 import io.airlift.airline.Arguments;
 import io.airlift.airline.Option;
 
+import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -51,7 +52,12 @@ public abstract class AbstractConfigCommand extends AbstractCommand {
 
         FileBackend backend = new LocalFileBackend();
 
-        String file = StringUtils.ensureEnd(
+        String file = StringUtils.ensureEnd(arguments().get(0), ".gyro");
+        if (!Paths.get(file).toFile().exists()) {
+            throw new BeamException(String.format("'%s' does not found.", file));
+        }
+
+        file = StringUtils.ensureEnd(
                 StringUtils.removeEnd(arguments().get(0), ".gyro"),
                 ".gyro.state");
 
