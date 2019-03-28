e724ec235544a1c49498804961287ac5190690da
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-03-28 17:15:55 -0400

    add list and map ingestion to ranges validation.

--- core/src/main/java/gyro/core/validation/RangesProcessor.java
+++ core/src/main/java/gyro/core/validation/RangesProcessor.java
@@ -2,6 +2,7 @@ package gyro.core.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class RangesProcessor extends RepeatableAnnotationBaseProcessor<Ranges> {
     private static RangesProcessor constructor = new RangesProcessor();
@@ -22,18 +23,38 @@ public class RangesProcessor extends RepeatableAnnotationBaseProcessor<Ranges> {
 
         List<String> rangesString = new ArrayList<>();
 
-        for (Range range : annotation.value()) {
-            annotationProcessor.initialize(range);
-            if (!annotationProcessor.isValid(value)) {
-                if (((RangeValidator) annotationProcessor).isDouble) {
-                    rangesString.add(String.format("[%s - %s]",
-                        range.low(),
-                        range.high()));
+        List<Object> values = new ArrayList<>();
+
+        if (value instanceof Number) {
+            values.add(value);
+        } else if (value instanceof List) {
+            values.addAll(((List) value));
+        } else if (value instanceof Map) {
+            values.addAll(((Map) value).keySet());
+        }
+
+        for (Object val : values) {
+            rangesString = new ArrayList<>();
+
+            for (Range range : annotation.value()) {
+                annotationProcessor.initialize(range);
+                if (!annotationProcessor.isValid(val)) {
+                    if (((RangeValidator) annotationProcessor).isDouble) {
+                        rangesString.add(String.format("[%s - %s]",
+                            range.low(),
+                            range.high()));
+                    } else {
+                        rangesString.add(String.format("[%s - %s]",
+                            (long) range.low(),
+                            (long) range.high()));
+                    }
                 } else {
-                    rangesString.add(String.format("[%s - %s]",
-                        (long) range.low(),
-                        (long) range.high()));
+                    break;
                 }
+            }
+
+            if (rangesString.size() == annotation.value().length) {
+                break;
             }
         }
 
