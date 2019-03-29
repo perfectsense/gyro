3acd694517a64378d0be36d3f74de1f0e71dcdc0
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-03-29 11:25:13 -0400

    add list and map ingestion to regex validation.

--- core/src/main/java/gyro/core/validation/AllowedRegexValidator.java
+++ core/src/main/java/gyro/core/validation/AllowedRegexValidator.java
@@ -1,6 +1,9 @@
 package gyro.core.validation;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class AllowedRegexValidator extends AnnotationBaseProcessor<AllowedRegex> {
     private static AllowedRegexValidator constructor = new AllowedRegexValidator();
@@ -14,20 +17,31 @@ public class AllowedRegexValidator extends AnnotationBaseProcessor<AllowedRegex>
 
     @Override
     boolean doValidation(Object value) {
-        String valueCheck = (String) value;
+        List<String> validValues = Arrays.asList(annotation.value());
+        List<String> valueChecks = new ArrayList<>();
 
-        for (String regex : annotation.value()) {
-            if (valueCheck.matches(regex)) {
-                return true;
-            }
+        if (value instanceof String) {
+            valueChecks.add((String) value);
+        } else if (value instanceof List && ((List) value).size() > 0 && ((List) value).get(0) instanceof String) {
+            valueChecks = (List<String>) value;
+        } else if (value instanceof Map && ((Map) value).keySet().size() > 0 && ((Map) value).keySet().toArray()[0] instanceof String) {
+            valueChecks = (List<String>) new ArrayList(Arrays.asList(((Map) value).keySet().toArray()));
         }
 
-        return false;
+        if (!valueChecks.isEmpty()) {
+            return valueChecks.stream().allMatch(o -> isValidString(o, validValues));
+        } else {
+            return true;
+        }
     }
 
     @Override
     public String getMessage() {
         return String.format(annotation.message(), annotation.display().length == 0
             ? Arrays.toString(annotation.value()) : Arrays.toString(annotation.display()));
+    }
+
+    private boolean isValidString(String valueCheck, List<String> validValues) {
+        return validValues.stream().anyMatch(valueCheck::matches);
     }
 }
--- core/src/main/java/gyro/core/validation/AllowedStringsValidator.java
+++ core/src/main/java/gyro/core/validation/AllowedStringsValidator.java
@@ -1,5 +1,6 @@
 package gyro.core.validation;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -18,14 +19,17 @@ public class AllowedStringsValidator extends AnnotationBaseProcessor<AllowedStri
     @Override
     boolean doValidation(Object value) {
         HashSet<String> validValues = new HashSet(Arrays.asList(annotation.value()));
+        List<String> valueChecks = new ArrayList<>();
         if (value instanceof String) {
-            String valueCheck = (String) value;
-
-            return Arrays.asList(annotation.value()).contains(valueCheck);
+           valueChecks.add((String) value);
         } else if (value instanceof List && ((List) value).size() > 0 && ((List) value).get(0) instanceof String) {
-            return ((List) value).stream().allMatch(validValues::contains);
+            valueChecks = (List<String>) value;
         } else if (value instanceof Map && ((Map) value).keySet().size() > 0 && ((Map) value).keySet().toArray()[0] instanceof String) {
-            return ((Map) value).keySet().stream().allMatch(validValues::contains);
+            valueChecks = (List<String>) new ArrayList(Arrays.asList(((Map) value).keySet().toArray()));
+        }
+
+        if (!valueChecks.isEmpty()) {
+            return validValues.containsAll(valueChecks);
         } else {
             return true;
         }
