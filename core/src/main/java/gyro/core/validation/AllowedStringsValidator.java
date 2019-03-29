506f460c75f7950087d603d28d74ddf4744ff3f9
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-03-29 10:51:33 -0400

    add list and map ingestion to allowed string validation.

--- core/src/main/java/gyro/core/validation/AllowedStringsValidator.java
+++ core/src/main/java/gyro/core/validation/AllowedStringsValidator.java
@@ -1,6 +1,9 @@
 package gyro.core.validation;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
 public class AllowedStringsValidator extends AnnotationBaseProcessor<AllowedStrings> {
     private static AllowedStringsValidator constructor = new AllowedStringsValidator();
@@ -14,9 +17,18 @@ public class AllowedStringsValidator extends AnnotationBaseProcessor<AllowedStri
 
     @Override
     boolean doValidation(Object value) {
-        String valueCheck = (String) value;
+        HashSet<String> validValues = new HashSet(Arrays.asList(annotation.value()));
+        if (value instanceof String) {
+            String valueCheck = (String) value;
 
-        return Arrays.asList(annotation.value()).contains(valueCheck);
+            return Arrays.asList(annotation.value()).contains(valueCheck);
+        } else if (value instanceof List && ((List) value).size() > 0 && ((List) value).get(0) instanceof String) {
+            return ((List) value).stream().allMatch(validValues::contains);
+        } else if (value instanceof Map && ((Map) value).keySet().size() > 0 && ((Map) value).keySet().toArray()[0] instanceof String) {
+            return ((Map) value).keySet().stream().allMatch(validValues::contains);
+        } else {
+            return true;
+        }
     }
 
     @Override
