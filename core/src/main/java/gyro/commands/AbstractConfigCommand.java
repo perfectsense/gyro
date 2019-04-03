7b2fa8a282e21b813962be2554bc70722d678700
Hao Sun <PSDhao@users.noreply.github.com>
2019-04-03 14:24:46 -0400

    Remove unused state imports after every change is successfully done

--- core/src/main/java/gyro/commands/AbstractConfigCommand.java
+++ core/src/main/java/gyro/commands/AbstractConfigCommand.java
@@ -94,7 +94,9 @@ public abstract class AbstractConfigCommand extends AbstractCommand {
             throw new BeamException(ex.getMessage());
         }
 
-        doExecute(current, pending, new State(pending, test));
+        State state = new State(pending, test);
+        doExecute(current, pending, state);
+        state.cleanUp();
     }
 
     private void refreshCredentials(FileScope scope) {
