f0b3b8501afcea3cfe74b1a4559aed301f656b01
Hao Sun <PSDhao@users.noreply.github.com>
2019-04-23 10:16:20 -0400

    Merge findRootDirectory into getRootInitFile

--- core/src/main/java/gyro/core/GyroCore.java
+++ core/src/main/java/gyro/core/GyroCore.java
@@ -3,12 +3,9 @@ package gyro.core;
 import com.psddev.dari.util.ObjectUtils;
 import com.psddev.dari.util.ThreadLocalStack;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 public class GyroCore {
 
@@ -35,36 +32,20 @@ public class GyroCore {
         return userHome;
     }
 
-    public static Path getRootInitFile() throws IOException {
-        Path rootDir = findRootDirectory(Paths.get("").toAbsolutePath());
-        if (rootDir != null) {
-            Path initFile = rootDir.resolve(Paths.get("init.gyro"));
-            if (Files.exists(initFile) && Files.isRegularFile(initFile)) {
-                return initFile;
-            }
-        }
-
-        throw new InitFileNotFoundException("Not a gyro project directory, use 'gyro init <plugins>...' to create one. "
-             + "See 'gyro help init' for detailed usage.");
-    }
-
-    private static Path findRootDirectory(Path path) throws IOException {
+    public static Path getRootInitFile() {
+        Path path = Paths.get("").toAbsolutePath();
         while (path != null) {
             if (Files.isDirectory(path)) {
-                try (Stream<Path> stream = Files.list(path)) {
-                    Optional<Path> optional = stream.filter(t -> Files.isDirectory(t)
-                        && t.getFileName() != null
-                        && t.getFileName().toString().equals(".gyro")).findAny();
-
-                    if (optional.isPresent()) {
-                        return optional.get().normalize();
-                    }
+                Path initFile = path.resolve(Paths.get(".gyro", "init.gyro"));
+                if (Files.exists(initFile) && Files.isRegularFile(initFile)) {
+                    return initFile;
                 }
             }
 
             path = path.getParent();
         }
 
-        return null;
+        throw new InitFileNotFoundException("Not a gyro project directory, use 'gyro init <plugins>...' to create one. "
+             + "See 'gyro help init' for detailed usage.");
     }
 }
