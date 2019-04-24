b93647c37b662ecc3fb7d336379ba825e64762cb
Hao Sun <PSDhao@users.noreply.github.com>
2019-04-24 13:34:42 -0400

    Change RootScope.load to be more logically correct

--- core/src/main/java/gyro/core/scope/RootScope.java
+++ core/src/main/java/gyro/core/scope/RootScope.java
@@ -157,7 +157,30 @@ public class RootScope extends FileScope {
     }
 
     public void load(FileBackend backend) throws Exception {
-        load(backend, null);
+        List<String> files = new ArrayList<>();
+
+        try {
+            Path gyroDir = GyroCore.getRootInitFile().getParent();
+            try (Stream<Path> pathStream = this.current != null
+                ? Files.find(gyroDir.getParent(), Integer.MAX_VALUE,
+                (p, b) -> b.isRegularFile()
+                    && p.toString().endsWith(".gyro")
+                    && !p.toString().startsWith(gyroDir.toString()))
+
+                : Files.find(gyroDir.resolve(Paths.get("state")), Integer.MAX_VALUE,
+                (p, b) -> b.isRegularFile()
+                    && p.toString().endsWith(".gyro.state"))) {
+
+                for (Path path : (Iterable<Path>) pathStream::iterator) {
+                    files.add(path.toString());
+                }
+            }
+
+        } catch (IOException e) {
+            throw new GyroException(e.getMessage(), e);
+        }
+
+        load(backend, files);
     }
 
     public void load(FileBackend backend, List<String> files) throws Exception {
@@ -166,27 +189,7 @@ public class RootScope extends FileScope {
         }
 
         if (files == null) {
-            files = new ArrayList<>();
-            try {
-                Path gyroDir = GyroCore.getRootInitFile().getParent();
-                try (Stream<Path> pathStream = this.current != null
-                    ? Files.find(gyroDir.getParent(), Integer.MAX_VALUE,
-                        (p, b) -> b.isRegularFile()
-                            && p.toString().endsWith(".gyro")
-                            && !p.toString().startsWith(gyroDir.toString()))
-
-                    : Files.find(gyroDir.resolve(Paths.get("state")), Integer.MAX_VALUE,
-                        (p, b) -> b.isRegularFile()
-                            && p.toString().endsWith(".gyro.state"))) {
-
-                    for (Path path : (Iterable<Path>) pathStream::iterator) {
-                        files.add(path.toString());
-                    }
-                }
-
-            } catch (IOException e) {
-                throw new GyroException(e.getMessage(), e);
-            }
+            files = Collections.emptyList();
         }
 
         List<Node> nodes = new ArrayList<>();
