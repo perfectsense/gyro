9120ea96a94ff7d4dd3a96d06d450afdd65581bd
Hyoo Lim <hyoolim@gmail.com>
2019-05-04 09:25:25 -0400

    Adds IfNodeTest.

--- lang/src/main/java/gyro/lang/ast/control/IfNode.java
+++ lang/src/main/java/gyro/lang/ast/control/IfNode.java
@@ -1,30 +1,43 @@
 package gyro.lang.ast.control;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
 import gyro.lang.ast.NodeVisitor;
 import gyro.lang.ast.Node;
 import gyro.parser.antlr4.GyroParser;
+import gyro.util.ImmutableCollectors;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class IfNode extends Node {
 
     private final List<Node> conditions;
     private final List<List<Node>> bodies;
 
-    public IfNode(GyroParser.IfStatementContext context) {
-        conditions = context.condition()
-            .stream()
-            .map(e -> Node.create(e))
-            .collect(Collectors.toList());
+    public IfNode(List<Node> conditions, List<List<Node>> bodies) {
+        this.conditions = ImmutableList.copyOf(Preconditions.checkNotNull(conditions));
 
-        bodies = context.blockBody()
+        this.bodies = Preconditions.checkNotNull(bodies)
             .stream()
-            .map(cbc -> cbc.blockStatement()
+            .map(ImmutableList::copyOf)
+            .collect(ImmutableCollectors.toList());
+    }
+
+    public IfNode(GyroParser.IfStatementContext context) {
+        this(
+            Preconditions.checkNotNull(context)
+                .condition()
                 .stream()
-                .map(csc -> Node.create(csc.getChild(0)))
-                .collect(Collectors.toList()))
-            .collect(Collectors.toList());
+                .map(Node::create)
+                .collect(ImmutableCollectors.toList()),
+
+            context.blockBody()
+                .stream()
+                .map(cbc -> cbc.blockStatement()
+                    .stream()
+                    .map(csc -> Node.create(csc.getChild(0)))
+                    .collect(ImmutableCollectors.toList()))
+                .collect(ImmutableCollectors.toList()));
     }
 
     public List<Node> getConditions() {
