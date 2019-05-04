7bf4fa94ff1a7ac7f410d79b251955b7519b6ed1
Hyoo Lim <hyoolim@gmail.com>
2019-05-04 14:24:10 -0400

    Renames ConditionNode to CompoundConditionNode for clarity.

--- lang/src/main/java/gyro/lang/ast/condition/AndConditionNode.java
+++ lang/src/main/java/gyro/lang/ast/condition/AndConditionNode.java
@@ -1,12 +1,16 @@
 package gyro.lang.ast.condition;
 
+import com.google.common.base.Preconditions;
+import gyro.lang.ast.Node;
 import gyro.lang.ast.NodeVisitor;
 import gyro.parser.antlr4.GyroParser;
 
-public class AndConditionNode extends ConditionNode {
+public class AndConditionNode extends CompoundConditionNode {
 
     public AndConditionNode(GyroParser.AndConditionContext context) {
-        super(context);
+        super(
+            Node.create(Preconditions.checkNotNull(context).getChild(0)),
+            Node.create(context.getChild(2)));
     }
 
     @Override
--- lang/src/main/java/gyro/lang/ast/condition/OrConditionNode.java
+++ lang/src/main/java/gyro/lang/ast/condition/OrConditionNode.java
@@ -1,12 +1,16 @@
 package gyro.lang.ast.condition;
 
+import com.google.common.base.Preconditions;
+import gyro.lang.ast.Node;
 import gyro.lang.ast.NodeVisitor;
 import gyro.parser.antlr4.GyroParser;
 
-public class OrConditionNode extends ConditionNode {
+public class OrConditionNode extends CompoundConditionNode {
 
     public OrConditionNode(GyroParser.OrConditionContext context) {
-        super(context);
+        super(
+            Node.create(Preconditions.checkNotNull(context).getChild(0)),
+            Node.create(context.getChild(2)));
     }
 
     @Override
