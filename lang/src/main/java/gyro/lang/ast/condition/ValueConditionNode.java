86760f6443c98981ea3e7418984858dfdda5b5e3
Hyoo Lim <hyoolim@gmail.com>
2019-05-04 15:50:23 -0400

    Changes ValueConditionNode to allow for list and map values.

--- lang/src/main/java/gyro/lang/ast/condition/ValueConditionNode.java
+++ lang/src/main/java/gyro/lang/ast/condition/ValueConditionNode.java
@@ -3,22 +3,21 @@ package gyro.lang.ast.condition;
 import com.google.common.base.Preconditions;
 import gyro.lang.ast.Node;
 import gyro.lang.ast.NodeVisitor;
-import gyro.lang.ast.value.ValueNode;
 import gyro.parser.antlr4.GyroParser;
 
 public class ValueConditionNode extends Node {
 
-    private final ValueNode value;
+    private final Node value;
 
-    public ValueConditionNode(ValueNode value) {
+    public ValueConditionNode(Node value) {
         this.value = Preconditions.checkNotNull(value);
     }
 
     public ValueConditionNode(GyroParser.ValueConditionContext context) {
-        this((ValueNode) Node.create(Preconditions.checkNotNull(context).value()));
+        this(Node.create(Preconditions.checkNotNull(context).value()));
     }
 
-    public ValueNode getValue() {
+    public Node getValue() {
         return value;
     }
 
--- lang/src/test/java/gyro/lang/ast/condition/OrConditionNodeTest.java
+++ lang/src/test/java/gyro/lang/ast/condition/OrConditionNodeTest.java
@@ -2,6 +2,7 @@ package gyro.lang.ast.condition;
 
 import gyro.lang.ast.AbstractNodeTest;
 import gyro.lang.ast.Node;
+import gyro.lang.ast.value.ValueNode;
 import gyro.parser.antlr4.GyroParser;
 import org.junit.jupiter.api.Test;
 
@@ -17,12 +18,16 @@ class OrConditionNodeTest extends AbstractNodeTest<OrConditionNode> {
         Node left = node.getLeft();
 
         assertThat(left).isInstanceOf(ValueConditionNode.class);
-        assertThat(((ValueConditionNode) left).getValue().getValue()).isEqualTo(Boolean.TRUE);
+        assertThat(getValue(left)).isEqualTo(Boolean.TRUE);
 
         Node right = node.getRight();
 
         assertThat(right).isInstanceOf(ValueConditionNode.class);
-        assertThat(((ValueConditionNode) right).getValue().getValue()).isEqualTo(Boolean.TRUE);
+        assertThat(getValue(right)).isEqualTo(Boolean.TRUE);
+    }
+
+    private Object getValue(Node node) {
+        return ((ValueNode) ((ValueConditionNode) node).getValue()).getValue();
     }
 
 }
