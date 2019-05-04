20dada97f1289c8b4cfbd86c162b768d029b9eff
Hyoo Lim <hyoolim@gmail.com>
2019-05-04 16:08:04 -0400

    Adds InterpolatedStringNodeTest.

--- lang/src/main/java/gyro/lang/ast/value/InterpolatedStringNode.java
+++ lang/src/main/java/gyro/lang/ast/value/InterpolatedStringNode.java
@@ -1,22 +1,34 @@
 package gyro.lang.ast.value;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
 import gyro.lang.ast.NodeVisitor;
 import gyro.lang.ast.Node;
 import gyro.parser.antlr4.GyroParser;
+import gyro.util.ImmutableCollectors;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class InterpolatedStringNode extends Node {
 
     private final List<Object> items;
 
+    public InterpolatedStringNode(List<Object> items) {
+        Preconditions.checkNotNull(items);
+
+        Preconditions.checkArgument(
+            items.stream().allMatch(i -> i instanceof Node || i instanceof String),
+            "All items must be an instance of Node or String!");
+
+        this.items = ImmutableList.copyOf(items);
+    }
+
     public InterpolatedStringNode(GyroParser.InterpolatedStringContext context) {
-        items = context.stringContent()
-                .stream()
-                .map(c -> c.getChild(0))
-                .map(c -> c instanceof GyroParser.ReferenceContext ? Node.create(c) : c.getText())
-                .collect(Collectors.toList());
+        this(context.stringContent()
+            .stream()
+            .map(c -> c.getChild(0))
+            .map(c -> c instanceof GyroParser.ReferenceContext ? Node.create(c) : c.getText())
+            .collect(ImmutableCollectors.toList()));
     }
 
     public List<Object> getItems() {
