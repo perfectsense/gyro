92e1fe2ae48b31d3315e48ac16b25ba1b7a4ffc3
Hyoo Lim <hyoolim@gmail.com>
2019-05-21 10:44:24 -0400

    Changes Finder to be an abstract class.

--- core/src/main/java/gyro/core/finder/Finder.java
+++ core/src/main/java/gyro/core/finder/Finder.java
@@ -6,9 +6,10 @@ import java.util.Map;
 import gyro.core.auth.Credentials;
 import gyro.core.resource.Resource;
 
-public interface Finder<R extends Resource> {
+public abstract class Finder<R extends Resource> {
 
-    List<R> findAll(Credentials credentials);
+    public abstract List<R> findAll(Credentials credentials);
 
-    List<R> find(Credentials credentials, Map<String, String> filters);
+    public abstract List<R> find(Credentials credentials, Map<String, String> filters);
+
 }
