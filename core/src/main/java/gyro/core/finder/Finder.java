78ddd715007c85e9d268da7c51c67b65afc0368f
Hyoo Lim <hyoolim@gmail.com>
2019-05-21 11:22:33 -0400

    Adds Finder.newResource.

--- core/src/main/java/gyro/core/finder/Finder.java
+++ core/src/main/java/gyro/core/finder/Finder.java
@@ -3,7 +3,10 @@ package gyro.core.finder;
 import java.util.List;
 import java.util.Map;
 
+import com.psddev.dari.util.TypeDefinition;
 import gyro.core.auth.Credentials;
+import gyro.core.resource.DiffableScope;
+import gyro.core.resource.DiffableType;
 import gyro.core.resource.Resource;
 import gyro.core.resource.Scope;
 
@@ -17,6 +20,15 @@ public abstract class Finder<R extends Resource> {
 
     public Credentials<?> credentials() {
         return Credentials.getInstance(getClass(), scope);
+    }
+
+    public R newResource() {
+        @SuppressWarnings("unchecked")
+        Class<R> resourceClass = (Class<R>) TypeDefinition.getInstance(getClass())
+            .getInferredGenericTypeArgumentClass(Finder.class, 0);
+
+        return DiffableType.getInstance(resourceClass)
+            .newDiffable(null, null, new DiffableScope(scope));
     }
 
 }
