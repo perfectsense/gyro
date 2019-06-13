1c2b171ee18890a07c2f960b1ff1007de2e1279a
Hyoo Lim <hyoolim@gmail.com>
2019-06-13 10:59:45 -0400

    Fixes extends directive to work with lists and resources.

--- core/src/main/java/gyro/core/resource/ExtendsDirectiveProcessor.java
+++ core/src/main/java/gyro/core/resource/ExtendsDirectiveProcessor.java
@@ -14,7 +14,6 @@ public class ExtendsDirectiveProcessor extends DirectiveProcessor {
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public void process(Scope scope, List<Object> arguments) {
         DiffableScope diffableScope = scope.getClosest(DiffableScope.class);
 
@@ -26,12 +25,36 @@ public class ExtendsDirectiveProcessor extends DirectiveProcessor {
             throw new GyroException("@extends directive only takes 1 argument!");
         }
 
-        Object source = arguments.get(0);
-        RootScope rootScope = scope.getRootScope();
+        processSource(diffableScope, arguments.get(0));
+    }
 
-        if (source instanceof String) {
+    @SuppressWarnings("unchecked")
+    private void processSource(DiffableScope scope, Object source) {
+        if (source instanceof List) {
+            List<Object> list = (List<Object>) source;
+            int size = list.size();
+
+            if (size != 1) {
+                throw new GyroException(String.format(
+                    "Can't extend from [%s] items!",
+                    size));
+            }
+
+            processSource(scope, list.get(0));
+
+        } else if (source instanceof Map) {
+            ((Map<String, Object>) source).forEach(scope::putIfAbsent);
+
+        } else if (source instanceof Resource) {
+            Resource resource = (Resource) source;
+
+            for (DiffableField field : DiffableType.getInstance(resource.getClass()).getFields()) {
+                scope.putIfAbsent(field.getName(), field.getValue(resource));
+            }
+
+        } else if (source instanceof String) {
             String name = (String) source;
-            Resource resource = rootScope.findResource(name);
+            Resource resource = scope.getRootScope().findResource(name);
 
             if (resource == null) {
                 throw new GyroException(String.format(
@@ -39,12 +62,7 @@ public class ExtendsDirectiveProcessor extends DirectiveProcessor {
                     name));
             }
 
-            for (DiffableField field : DiffableType.getInstance(resource.getClass()).getFields()) {
-                diffableScope.putIfAbsent(field.getName(), field.getValue(resource));
-            }
-
-        } else if (source instanceof Map) {
-            ((Map<String, Object>) source).forEach(diffableScope::putIfAbsent);
+            processSource(scope, resource);
         }
     }
 
