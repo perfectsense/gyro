df725603aba79b9a9995b42179dd9864c2615867
Hyoo Lim <hyoolim@gmail.com>
2019-06-20 14:11:34 -0400

    Optimizes FinderReferenceResolver to make use of Finder.find method.

--- core/src/main/java/gyro/core/reference/FinderReferenceResolver.java
+++ core/src/main/java/gyro/core/reference/FinderReferenceResolver.java
@@ -1,13 +1,20 @@
 package gyro.core.reference;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import gyro.core.GyroException;
 import gyro.core.finder.Finder;
 import gyro.core.finder.FinderSettings;
 import gyro.core.finder.FinderType;
+import gyro.core.resource.NodeEvaluator;
 import gyro.core.resource.Resource;
 import gyro.core.resource.Scope;
+import gyro.lang.query.AndQuery;
+import gyro.lang.query.ComparisonQuery;
 import gyro.lang.query.Query;
 
 public class FinderReferenceResolver extends ReferenceResolver {
@@ -32,9 +39,55 @@ public class FinderReferenceResolver extends ReferenceResolver {
                 type));
         }
 
-        return FinderType.getInstance(finderClass)
-            .newInstance(scope)
-            .findAll();
+        Finder<Resource> finder = FinderType.getInstance(finderClass).newInstance(scope);
+        List<Resource> resources = null;
+
+        if (arguments.isEmpty() && !queries.isEmpty()) {
+            Map<String, String> filters = getFilters(scope, queries);
+
+            if (!filters.isEmpty()) {
+                resources = finder.find(filters);
+            }
+        }
+
+        if (resources == null) {
+            resources = finder.findAll();
+        }
+
+        return ReferenceResolver.resolveRemaining(
+            scope,
+            arguments,
+            queries,
+            resources);
+    }
+
+    private Map<String, String> getFilters(Scope scope, List<Query> queries) {
+        NodeEvaluator evaluator = scope.getRootScope().getEvaluator();
+        Map<String, String> filters = new HashMap<>();
+
+        for (Iterator<Query> i = queries.iterator(); i.hasNext(); ) {
+            Query query = i.next();
+
+            if (query instanceof AndQuery) {
+                List<Query> childQueries = new ArrayList<>(((AndQuery) query).getChildren());
+                Map<String, String> childFilters = getFilters(scope, childQueries);
+
+                if (childQueries.isEmpty() && !childFilters.isEmpty()) {
+                    filters.putAll(childFilters);
+                    i.remove();
+                }
+
+            } else if (query instanceof ComparisonQuery) {
+                ComparisonQuery comparison = (ComparisonQuery) query;
+
+                if ("=".equals(comparison.getOperator())) {
+                    filters.put(comparison.getKey(), (String) evaluator.visit(comparison.getValue(), scope));
+                    i.remove();
+                }
+            }
+        }
+
+        return filters;
     }
 
 }
