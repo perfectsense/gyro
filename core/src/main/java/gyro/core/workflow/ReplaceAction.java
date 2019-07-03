c6991bc8d3838c3b962918b88c111f6cf8f2dd2c
Hyoo Lim <hyoolim@gmail.com>
2019-07-03 10:32:05 -0400

    Replaces swap directive with replace.

---
+++ core/src/main/java/gyro/core/workflow/ReplaceAction.java
@@ -0,0 +1,65 @@
+package gyro.core.workflow;
+
+import com.google.common.base.Preconditions;
+import gyro.core.GyroException;
+import gyro.core.GyroUI;
+import gyro.core.resource.NodeEvaluator;
+import gyro.core.resource.Resource;
+import gyro.core.resource.RootScope;
+import gyro.core.resource.Scope;
+import gyro.core.resource.State;
+import gyro.lang.ast.Node;
+
+public class ReplaceAction extends Action {
+
+    private final Node resource;
+    private final Node with;
+
+    public ReplaceAction(Node resource, Node with) {
+        this.resource = Preconditions.checkNotNull(resource);
+        this.with = Preconditions.checkNotNull(with);
+    }
+
+    public Node getResource() {
+        return resource;
+    }
+
+    public Node getWith() {
+        return with;
+    }
+
+    @Override
+    public void execute(GyroUI ui, State state, RootScope pending, Scope scope) {
+        NodeEvaluator evaluator = scope.getRootScope().getEvaluator();
+        Object resource = evaluator.visit(this.resource, scope);
+
+        if (resource == null) {
+            throw new GyroException("Can't replace because the first argument is null!");
+        }
+
+        if (!(resource instanceof Resource)) {
+            throw new GyroException(String.format(
+                "Can't replace the first argument [%s] because it's not a resource!",
+                resource));
+        }
+
+        Object with = evaluator.visit(this.with, scope);
+
+        if (with == null) {
+            throw new GyroException("Can't replace because the second argument is null!");
+        }
+
+        if (!(with instanceof Resource)) {
+            throw new GyroException(String.format(
+                "Can't replace the second argument [%s] because it's not a resource!",
+                with));
+        }
+
+        Resource resourceResource = (Resource) resource;
+        Resource withResource = (Resource) with;
+
+        ui.write("@|magenta ⤢ Replacing %s with %s|@\n", resourceResource.name(), withResource.name());
+        state.replace(resourceResource, withResource);
+    }
+
+}
