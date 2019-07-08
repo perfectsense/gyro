d0447c5f7cb346998b70c2c96ed682824fedbed0
Hyoo Lim <hyoolim@gmail.com>
2019-07-08 14:04:57 -0400

    Adds node field to GyroException.

--- core/src/main/java/gyro/core/GyroException.java
+++ core/src/main/java/gyro/core/GyroException.java
@@ -1,13 +1,38 @@
 package gyro.core;
 
+import gyro.lang.ast.Node;
+
 public class GyroException extends RuntimeException {
 
-    public GyroException(String message) {
-        super(message);
+    private final Node node;
+
+    public GyroException(Node node, String message, Throwable cause) {
+        super(message, cause);
+        this.node = node;
+    }
+
+    public GyroException(Node node, String message) {
+        this(node, message, null);
+    }
+
+    public GyroException(Node node, Throwable cause) {
+        this(node, null, cause);
     }
 
     public GyroException(String message, Throwable cause) {
-        super(message, cause);
+        this(null, message, cause);
+    }
+
+    public GyroException(String message) {
+        this(null, message, null);
+    }
+
+    public GyroException(Throwable cause) {
+        this(null, null, cause);
+    }
+
+    public Node getNode() {
+        return node;
     }
 
 }
