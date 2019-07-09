43b832bc64e2c9fb0906fc374a859f9c79243ccb
Hyoo Lim <hyoolim@gmail.com>
2019-07-09 16:11:12 -0400

    Improves error handling in ReferencePlugin.

--- core/src/main/java/gyro/core/reference/ReferencePlugin.java
+++ core/src/main/java/gyro/core/reference/ReferencePlugin.java
@@ -1,14 +1,15 @@
 package gyro.core.reference;
 
+import gyro.core.Reflections;
 import gyro.core.plugin.Plugin;
 import gyro.core.resource.RootScope;
 
 public class ReferencePlugin extends Plugin {
 
     @Override
-    public void onEachClass(RootScope root, Class<?> aClass) throws Exception {
+    public void onEachClass(RootScope root, Class<?> aClass) {
         if (ReferenceResolver.class.isAssignableFrom(aClass)) {
-            ReferenceResolver resolver = (ReferenceResolver) aClass.newInstance();
+            ReferenceResolver resolver = (ReferenceResolver) Reflections.newInstance(aClass);
 
             root.getSettings(ReferenceSettings.class)
                 .getResolvers()
