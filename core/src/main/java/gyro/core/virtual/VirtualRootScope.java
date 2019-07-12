03a8e81fa0f7e7cabac3ca6c563d28613efd6aa3
Hao Sun <PSDhao@users.noreply.github.com>
2019-07-12 13:59:45 -0400

    Simply find resource logic

--- core/src/main/java/gyro/core/virtual/VirtualRootScope.java
+++ core/src/main/java/gyro/core/virtual/VirtualRootScope.java
@@ -4,8 +4,6 @@ import com.google.common.collect.ImmutableSet;
 import gyro.core.resource.Resource;
 import gyro.core.resource.RootScope;
 
-import java.util.stream.Stream;
-
 public class VirtualRootScope extends RootScope {
 
     private final String prefix;
@@ -21,13 +19,7 @@ public class VirtualRootScope extends RootScope {
     public Resource findResource(String fullName) {
         String[] names = fullName.split("::");
         names[names.length - 1] = prefix + "/" + names[names.length - 1];
-
-        return Stream.concat(Stream.of(this), getFileScopes().stream())
-            .map(s -> s.get(String.join("::", names)))
-            .filter(Resource.class::isInstance)
-            .map(Resource.class::cast)
-            .findFirst()
-            .orElse(null);
+        return super.findResource(String.join("::", names));
     }
 
     @Override
