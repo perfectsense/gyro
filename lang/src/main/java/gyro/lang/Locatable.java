fe75381752f74e520dd71f0c63a3492ad3707c0c
Hyoo Lim <hyoolim@gmail.com>
2019-07-22 13:38:43 -0400

    Adds Locatable.getStream so that toCodeSnippet can be auto provided.

--- lang/src/main/java/gyro/lang/Locatable.java
+++ lang/src/main/java/gyro/lang/Locatable.java
@@ -1,8 +1,13 @@
 package gyro.lang;
 
+import java.util.Optional;
+
+import org.antlr.v4.runtime.IntStream;
+import org.apache.commons.lang3.StringUtils;
+
 public interface Locatable {
 
-    String getFile();
+    GyroCharStream getStream();
 
     int getStartLine();
 
@@ -11,6 +16,12 @@ public interface Locatable {
     int getStopLine();
 
     int getStopColumn();
+
+    default String getFile() {
+        return Optional.ofNullable(getStream())
+            .map(GyroCharStream::getSourceName)
+            .orElse(IntStream.UNKNOWN_SOURCE_NAME);
+    }
 
     default String toLocation() {
         int startLine = getStartLine();
@@ -43,6 +54,62 @@ public interface Locatable {
         }
     }
 
-    String toCodeSnippet();
+    default String toCodeSnippet() {
+        GyroCharStream stream = getStream();
+
+        if (stream == null) {
+            return null;
+        }
+
+        int startLine = getStartLine();
+        int startColumn = getStartColumn();
+        int stopLine = getStopLine();
+        int stopColumn = getStopColumn();
+
+        StringBuilder text = new StringBuilder();
+        int previousLine = startLine - 1;
+        String previous = stream.getLineText(previousLine);
+        String format = "%" + String.valueOf(stopLine + 1).length() + "d: ";
+
+        if (!StringUtils.isBlank(previous)) {
+            text.append(String.format(format, previousLine + 1));
+            text.append(previous);
+            text.append('\n');
+        }
+
+        for (int line = startLine; line <= stopLine; line ++) {
+            String current = stream.getLineText(line);
+            int end = current.length();
+            int start = line == startLine ? startColumn : 0;
+            int stop = line == stopLine ? stopColumn + 1 : end;
+
+            text.append(String.format(format, line + 1));
+
+            if (start > 1) {
+                text.append(current, 0, start);
+            }
+
+            text.append("@|red,underline ");
+            text.append(current, start, stop > end ? end : stop);
+            text.append("|@");
+
+            if (end > stop) {
+                text.append(current, stop, end);
+            }
+
+            text.append('\n');
+        }
+
+        int nextLine = stopLine + 1;
+        String next = stream.getLineText(nextLine);
+
+        if (!StringUtils.isBlank(next)) {
+            text.append(String.format(format, nextLine + 1));
+            text.append(next);
+            text.append('\n');
+        }
+
+        return text.toString();
+    }
 
 }
