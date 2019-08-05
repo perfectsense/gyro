d03a751c5828d08a48d950cb6a401917f80cafce
Jeremy Collins <jeremy@perfectsensedigital.com>
2019-08-05 15:11:56 -0400

    Store backend class with namespace prefix

--- core/src/main/java/gyro/core/backend/FileBackendPlugin.java
+++ core/src/main/java/gyro/core/backend/FileBackendPlugin.java
@@ -1,6 +1,7 @@
 package gyro.core.backend;
 
 import gyro.core.FileBackend;
+import gyro.core.NamespaceUtils;
 import gyro.core.Type;
 import gyro.core.plugin.Plugin;
 import gyro.core.scope.RootScope;
@@ -10,21 +11,19 @@ import java.util.Optional;
 public class FileBackendPlugin extends Plugin {
     @Override
     public void onEachClass(RootScope root, Class<?> aClass) throws Exception {
-
         if (FileBackend.class.isAssignableFrom(aClass)) {
             @SuppressWarnings("unchecked")
             Class<? extends FileBackend> fileBackendClass = (Class<? extends FileBackend>) aClass;
-
+            String namespacePrefix = NamespaceUtils.getNamespacePrefix(fileBackendClass);
 
             String type = Optional.ofNullable(fileBackendClass.getAnnotation(Type.class))
                     .map(Type::value)
                     .orElse(null);
 
-                root.getSettings(FileBackendSettings.class)
-                        .getFileBackendClasses()
-                        .put(type, fileBackendClass);
+            root.getSettings(FileBackendSettings.class)
+                    .getFileBackendClasses()
+                    .put(namespacePrefix + type, fileBackendClass);
 
         }
-
     }
 }
