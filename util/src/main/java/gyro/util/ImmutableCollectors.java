5aa73ad63608efb32ff5de09921205a3ea61c32f
Hyoo Lim <hyoolim@gmail.com>
2019-08-22 13:40:44 -0400

    Adds ImmutableCollectors.toMap.

--- util/src/main/java/gyro/util/ImmutableCollectors.java
+++ util/src/main/java/gyro/util/ImmutableCollectors.java
@@ -1,9 +1,13 @@
 package gyro.util;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collector;
 
+import com.google.common.base.Functions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
 public class ImmutableCollectors {
 
@@ -16,6 +20,24 @@ public class ImmutableCollectors {
                 return x;
             },
             ImmutableList.Builder::build);
+    }
+
+    public static <T, K, V> Collector<T, ?, Map<K, V>> toMap(
+        Function<? super T, ? extends K> keyMapper,
+        Function<? super T, ? extends V> valueMapper) {
+
+        return Collector.of(
+            ImmutableMap::<K, V>builder,
+            (map, item) -> map.put(keyMapper.apply(item), valueMapper.apply(item)),
+            (x, y) -> {
+                x.putAll(y.build());
+                return x;
+            },
+            ImmutableMap.Builder::build);
+    }
+
+    public static <T, K> Collector<T, ?, Map<K, T>> toMap(Function<? super T, ? extends K> keyMapper) {
+        return toMap(keyMapper, Functions.identity());
     }
 
 }
