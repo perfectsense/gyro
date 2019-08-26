3d5767e23158c389f28bceb5747b2a0a8274eeff
Hyoo Lim <hyoolim@gmail.com>
2019-08-26 16:03:56 -0400

    Renames DiffableScope.getNode to getBlock for clarity.

--- core/src/main/java/gyro/core/scope/DiffableScope.java
+++ core/src/main/java/gyro/core/scope/DiffableScope.java
@@ -7,25 +7,26 @@ import java.util.Map;
 import java.util.Optional;
 
 import gyro.lang.ast.Node;
+import gyro.lang.ast.block.BlockNode;
 
 public class DiffableScope extends Scope {
 
-    private final Node node;
+    private final BlockNode block;
     private List<Node> stateNodes = new ArrayList<>();
 
-    public DiffableScope(Scope parent, Node node) {
+    public DiffableScope(Scope parent, BlockNode block) {
         super(parent);
-        this.node = node;
+        this.block = block;
     }
 
-    public Node getNode() {
-        if (node != null) {
-            return node;
+    public BlockNode getBlock() {
+        if (block != null) {
+            return block;
 
         } else {
             return Optional.ofNullable(getParent())
                 .map(p -> p.getClosest(DiffableScope.class))
-                .map(DiffableScope::getNode)
+                .map(DiffableScope::getBlock)
                 .orElse(null);
         }
     }
