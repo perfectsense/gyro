c61fb2f94526b1c64abbaaad148a5ec04f72cb6e
Hyoo Lim <hyoolim@gmail.com>
2019-08-28 16:58:04 -0400

    Changes extends directive to recursively merge source values.

--- core/src/main/java/gyro/core/resource/ExtendsDirectiveProcessor.java
+++ core/src/main/java/gyro/core/resource/ExtendsDirectiveProcessor.java
@@ -1,11 +1,13 @@
 package gyro.core.resource;
 
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import gyro.core.GyroException;
 import gyro.core.Type;
@@ -17,30 +19,61 @@ import gyro.lang.ast.block.DirectiveNode;
 public class ExtendsDirectiveProcessor extends DirectiveProcessor<DiffableScope> {
 
     @Override
+    @SuppressWarnings("unchecked")
     public void process(DiffableScope scope, DirectiveNode node) {
         validateArguments(node, 1, 1);
-        processSource(scope, getArgument(scope, node, Object.class, 0));
-    }
 
-    @SuppressWarnings("unchecked")
-    private void processSource(DiffableScope scope, Object source) {
-        if (source instanceof Map) {
-            ((Map<String, Object>) source).forEach((key, value) -> scope.computeIfAbsent(key, k -> clone(value)));
+        Object source = getArgument(scope, node, Object.class, 0);
+        Map<String, Object> sourceMap;
+
+        if (source == null) {
+            throw new GyroException("Can't extend from a null!");
+
+        } else if (source instanceof Map) {
+            sourceMap = (Map<String, Object>) source;
 
         } else if (source instanceof Resource) {
-            processSource(scope, DiffableInternals.getScope((Resource) source));
+            sourceMap = DiffableInternals.getScope((Resource) source);
 
-        } else if (source instanceof String) {
-            String name = (String) source;
-            Resource resource = scope.getRootScope().findResource(name);
+        } else {
+            throw new GyroException(String.format(
+                "Can't extend from @|bold %s|@ because it's an instance of @|bold %s|@!",
+                source,
+                source.getClass().getName()));
+        }
 
-            if (resource == null) {
-                throw new GyroException(String.format(
-                    "Can't extend from @|bold %s|@ resource because it doesn't exist!",
-                    name));
-            }
+        sourceMap.forEach((key, value) -> scope.put(key, merge(scope.get(key), value)));
+    }
 
-            processSource(scope, resource);
+    private Object merge(Object oldValue, Object newValue) {
+        if (oldValue instanceof Collection && newValue instanceof Collection) {
+            @SuppressWarnings("unchecked")
+            Collection<Object> clone = (Collection<Object>) clone(oldValue);
+
+            ((Collection<?>) newValue).stream()
+                .map(this::clone)
+                .forEach(clone::add);
+
+            return clone;
+
+        } else if (oldValue instanceof Map && newValue instanceof Map) {
+            Map<?, ?> oldMap = (Map<?, ?>) oldValue;
+            Map<?, ?> newMap = (Map<?, ?>) newValue;
+
+            return Stream.of(oldMap, newMap)
+                .map(Map::keySet)
+                .flatMap(Set::stream)
+                .distinct()
+                .collect(
+                    LinkedHashMap::new,
+                    (map, key) -> map.put(clone(key), merge(clone(oldMap.get(key)), newMap.get(key))),
+                    Map::putAll);
+
+        } else if (oldValue != null) {
+            return oldValue;
+
+        } else {
+            return clone(newValue);
         }
     }
 
