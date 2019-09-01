91ef1d836c58d74f0db83ce2a4e5b257370c3b9e
Hao Sun <PSDhao@users.noreply.github.com>
2019-09-01 09:55:09 -0400

    Add modification processor update

--- core/src/main/java/gyro/core/diff/ModificationProcessor.java
+++ core/src/main/java/gyro/core/diff/ModificationProcessor.java
@@ -4,11 +4,14 @@ import gyro.core.GyroUI;
 import gyro.core.resource.Diffable;
 import gyro.core.resource.DiffableField;
 import gyro.core.resource.DiffableInternals;
+import gyro.core.resource.ModificationField;
 import gyro.core.resource.Resource;
 import gyro.core.scope.State;
 
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ModificationProcessor extends ChangeProcessor {
 
@@ -24,7 +27,22 @@ public class ModificationProcessor extends ChangeProcessor {
 
     @Override
     public void afterUpdate(GyroUI ui, State state, Resource current, Resource pending, Set<DiffableField> changedFields) throws Exception {
+        Map<Diffable, ModificationField> fieldByDiffable = DiffableInternals.getModificationByField(pending)
+            .entrySet()
+            .stream()
+            .filter(e -> changedFields.contains(e.getKey()))
+            .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
 
+        for (Map.Entry<Diffable, ModificationField> entry : fieldByDiffable.entrySet()) {
+            if (entry.getKey() instanceof Resource) {
+                Resource currentModification = (Resource) DiffableInternals.getModificationByField(current).get(entry.getValue());
+                ((Resource) entry.getKey()).update(ui, state, currentModification, changedFields
+                    .stream()
+                    .map(DiffableField::getName)
+                    .collect(Collectors.toSet())
+                );
+            }
+        }
     }
 
     @Override
