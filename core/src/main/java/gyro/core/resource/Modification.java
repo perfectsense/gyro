826834a33ee6dbcfada76192564f06651476f1fc
Jeremy Collins <jeremy@brightspot.com>
2019-09-12 13:49:27 -0400

    Make Modification have it's own method signatures

--- core/src/main/java/gyro/core/resource/Modification.java
+++ core/src/main/java/gyro/core/resource/Modification.java
@@ -1,11 +1,32 @@
 package gyro.core.resource;
 
-import gyro.core.diff.ChangeProcessor;
+import gyro.core.GyroUI;
+import gyro.core.scope.State;
 
-public abstract class Modification<T> extends Diffable implements ChangeProcessor {
+import java.util.Set;
+
+public abstract class Modification<T> extends Diffable {
 
     public boolean refresh(T current) {
         return false;
+    }
+
+    public void beforeCreate(GyroUI ui, State state, T resource) throws Exception {
+    }
+
+    public void afterCreate(GyroUI ui, State state, T resource) throws Exception {
+    }
+
+    public void beforeUpdate(GyroUI ui, State state, T current, T pending, Set<DiffableField> changedFields) throws Exception {
+    }
+
+    public void afterUpdate(GyroUI ui, State state, T current, T pending, Set<DiffableField> changedFields) throws Exception {
+    }
+
+    public void beforeDelete(GyroUI ui, State state, T resource) throws Exception {
+    }
+
+    public void afterDelete(GyroUI ui, State state, T resource) throws Exception {
     }
 
 }
