27ed3b1d6be49ee2281b9054d5e9693cfc9b912e
Jeremy Collins <jeremy@brightspot.com>
2019-09-13 06:15:27 -0400

    Allow modification fields to be validated

--- core/src/main/java/gyro/core/resource/ModificationField.java
+++ core/src/main/java/gyro/core/resource/ModificationField.java
@@ -1,10 +1,5 @@
 package gyro.core.resource;
 
-import gyro.core.validation.ValidationError;
-
-import java.util.Collections;
-import java.util.List;
-
 public class ModificationField extends DiffableField {
 
     public ModificationField(DiffableField field) {
@@ -21,11 +16,6 @@ public class ModificationField extends DiffableField {
     public void setValue(Diffable diffable, Object value) {
         Diffable modification = DiffableInternals.getModifications(diffable).get(this);
         super.setValue(modification, value);
-    }
-
-    @Override
-    public List<ValidationError> validate(Diffable diffable) {
-        return Collections.emptyList();
     }
 
 }
