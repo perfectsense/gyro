042d939593353fba6d08d53f7e95a51760a6fc82
Hyoo Lim <hyoolim@gmail.com>
2019-09-16 11:06:36 -0400

    Renames abstract method arguments to be more clear.

--- core/src/main/java/gyro/core/resource/Modification.java
+++ core/src/main/java/gyro/core/resource/Modification.java
@@ -10,10 +10,10 @@ public abstract class Modification<T extends Diffable> extends Diffable {
     public void refresh(T current) {
     }
 
-    public void beforeCreate(GyroUI ui, State state, T resource) throws Exception {
+    public void beforeCreate(GyroUI ui, State state, T pending) throws Exception {
     }
 
-    public void afterCreate(GyroUI ui, State state, T resource) throws Exception {
+    public void afterCreate(GyroUI ui, State state, T pending) throws Exception {
     }
 
     public void beforeUpdate(GyroUI ui, State state, T current, T pending, Set<DiffableField> changedFields) throws Exception {
@@ -22,10 +22,10 @@ public abstract class Modification<T extends Diffable> extends Diffable {
     public void afterUpdate(GyroUI ui, State state, T current, T pending, Set<DiffableField> changedFields) throws Exception {
     }
 
-    public void beforeDelete(GyroUI ui, State state, T resource) throws Exception {
+    public void beforeDelete(GyroUI ui, State state, T current) throws Exception {
     }
 
-    public void afterDelete(GyroUI ui, State state, T resource) throws Exception {
+    public void afterDelete(GyroUI ui, State state, T current) throws Exception {
     }
 
 }
