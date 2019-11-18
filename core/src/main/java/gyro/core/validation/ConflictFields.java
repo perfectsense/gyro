45919b2d8cdc97843945f92e562a7b555ef0bf91
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-11-18 16:10:26 -0500

    Add ConflictFields annotation and validator
    Allowing the use of multiple '@ConflictField' annotation on a method

--- core/src/main/java/gyro/core/validation/ConflictFields.java
+++ core/src/main/java/gyro/core/validation/ConflictFields.java
@@ -2,17 +2,14 @@ package gyro.core.validation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Documented
-@Repeatable(ConflictFields.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-@ValidatorClass(ConflictFieldValidator.class)
-public @interface ConflictField {
-    String source();
-    String conflict();
+@ValidatorClass(ConflictFieldsValidator.class)
+public @interface ConflictFields {
+    ConflictField[] value();
 }
