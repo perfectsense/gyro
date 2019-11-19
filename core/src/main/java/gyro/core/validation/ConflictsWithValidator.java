7e47d0cca4e9d1f2cc48f27b5274bc91aabf2af4
Deepanjan Bhattacharyya <deepanjan@perfectsensedigital.com>
2019-11-19 15:24:47 -0500

    Clean up code
    1. Use ObjectUtils.isBlank() to check for empty field values
    2. Get rid of isFieldEmpty and isValueEmpty in favor of the above
    3. Rename param value to fieldValue to avoid confusion between annotation.values()

--- core/src/main/java/gyro/core/validation/ConflictsWithValidator.java
+++ core/src/main/java/gyro/core/validation/ConflictsWithValidator.java
@@ -1,36 +1,27 @@
 package gyro.core.validation;
 
+import com.psddev.dari.util.ObjectUtils;
 import gyro.core.resource.Diffable;
 import gyro.core.resource.DiffableField;
 import gyro.core.resource.DiffableType;
 
-import java.util.Collection;
-import java.util.Map;
 import java.util.stream.Stream;
 
 public class ConflictsWithValidator implements Validator<ConflictsWith> {
     @Override
-    public boolean isValid(Diffable diffable, ConflictsWith annotation, Object value) {
+    public boolean isValid(Diffable diffable, ConflictsWith annotation, Object fieldValue) {
         DiffableType<Diffable> diffableType = DiffableType.getInstance(diffable);
 
-        return  (isValueEmpty(value) || Stream.of(annotation.value()).allMatch(o -> isFieldEmpty(o, diffableType, diffable)));
+        return  (ObjectUtils.isBlank(fieldValue) || Stream.of(annotation.value())
+            .allMatch(name -> {
+                DiffableField field = diffableType.getField(name);
+                return field == null || ObjectUtils.isBlank(field.getValue(diffable));
+            }));
     }
 
     @Override
     public String getMessage(ConflictsWith annotation) {
-        return String.format("Cannot be set when any of '%s' is set."
+        return String.format("Cannot be set when any of the following field(s) are set: ['%s']"
             , String.join("', '", annotation.value()));
-    }
-
-    private boolean isFieldEmpty(String name, DiffableType<Diffable> diffableType, Diffable diffable) {
-        DiffableField field = diffableType.getField(name);
-
-        return field == null || isValueEmpty(field.getValue(diffable));
-    }
-
-    private boolean isValueEmpty(Object value) {
-        return  (value == null)
-            || ((value instanceof Collection) && ((Collection<?>) value).isEmpty())
-            || ((value instanceof Map) && ((Map<?, ?>) value).isEmpty());
     }
 }
