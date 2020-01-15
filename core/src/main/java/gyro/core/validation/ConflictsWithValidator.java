7e1e278bd1786bb2394d554f01a31cb1205e82d3
Andrew Sung <asung@perfectsensedigital.com>
2020-01-15 14:55:09 -0500

    Fixes `ConflictsWith` validator to ignore non-configured fields.

--- core/src/main/java/gyro/core/validation/ConflictsWithValidator.java
+++ core/src/main/java/gyro/core/validation/ConflictsWithValidator.java
@@ -1,23 +1,30 @@
 package gyro.core.validation;
 
+import java.util.Set;
 import java.util.stream.Stream;
 
 import com.psddev.dari.util.ObjectUtils;
 import gyro.core.resource.Diffable;
 import gyro.core.resource.DiffableField;
+import gyro.core.resource.DiffableInternals;
 import gyro.core.resource.DiffableType;
 
 public class ConflictsWithValidator implements Validator<ConflictsWith> {
 
     @Override
     public boolean isValid(Diffable diffable, ConflictsWith annotation, Object fieldValue) {
+        if (ObjectUtils.isBlank(fieldValue)) {
+            return true;
+        }
+        Set<String> configuredFields = DiffableInternals.getConfiguredFields(diffable);
         DiffableType<Diffable> diffableType = DiffableType.getInstance(diffable);
 
-        return (ObjectUtils.isBlank(fieldValue) || Stream.of(annotation.value())
+        return Stream.of(annotation.value())
+            .filter(name -> configuredFields.contains(name))
             .allMatch(name -> {
                 DiffableField field = diffableType.getField(name);
                 return field == null || ObjectUtils.isBlank(field.getValue(diffable));
-            }));
+            });
     }
 
     @Override
