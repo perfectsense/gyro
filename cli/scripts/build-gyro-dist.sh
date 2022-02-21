e8a20130d26283d49c1bf66ee603889e0cf9881c
Jeremy Collins <jeremy@brightspot.com>
2022-02-21 12:05:25 -0500

    Stop distributing JRE with Gyro
    This causes issue on macOS when it's not signed.

--- cli/scripts/build-gyro-dist.sh
+++ cli/scripts/build-gyro-dist.sh
@@ -5,9 +5,4 @@ rm -rf dist/gyro-rt
 
 cd dist
 
-jlink --no-header-files \
-    --no-man-pages \
-    --add-modules java.logging,java.management,java.naming,java.scripting,java.xml,jdk.unsupported,jdk.xml.dom,java.desktop,java.instrument,java.compiler,java.sql,jdk.crypto.ec \
-    --output gyro-rt
-
-zip -r gyro-cli-${OS_NAME}-${VERSION}.zip gyro-rt gyro
+zip -r gyro-cli-${OS_NAME}-${VERSION}.zip gyro
